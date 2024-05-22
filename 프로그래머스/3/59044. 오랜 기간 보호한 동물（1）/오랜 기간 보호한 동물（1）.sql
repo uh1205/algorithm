b1fe0880b68e4d002809a30564921a40ab4332a5
Unho Yeo <yuh001205@gmail.com>
2024-05-22 09:35:24 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,5 @@
 select NAME, DATETIME
-from ANIMAL_INS i
-where i.ANIMAL_ID not in (select ANIMAL_ID from ANIMAL_OUTS)
+from ANIMAL_INS
+where ANIMAL_ID not in (select ANIMAL_ID from ANIMAL_OUTS)
 order by 2
 limit 3
