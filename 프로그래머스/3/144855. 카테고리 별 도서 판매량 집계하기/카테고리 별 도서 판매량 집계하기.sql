e506291361f66d5f280f30e5bcc81b31ff8762a9
Unho Yeo <yuh001205@gmail.com>
2024-05-23 09:16:58 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,5 +1,5 @@
 select CATEGORY, sum(SALES) TOTAL_SALES
 from BOOK b inner join BOOK_SALES s on b.BOOK_ID = s.BOOK_ID
-where s.SALES_DATE like '2022-01%'
+where SALES_DATE like '2022-01%'
 group by 1
 order by 1
