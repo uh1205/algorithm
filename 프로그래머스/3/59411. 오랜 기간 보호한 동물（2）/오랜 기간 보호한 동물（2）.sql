6bbd47d96c1f98f092479b6c56f8f2d6a9462257
Unho Yeo <yuh001205@gmail.com>
2024-05-26 16:46:51 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,4 +1,4 @@
 select o.ANIMAL_ID, o.NAME
 from ANIMAL_INS i join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
-order by datediff(o.DATETIME, i.DATETIME) desc
+order by timestampdiff(second, i.DATETIME, o.DATETIME) desc
 limit 2
