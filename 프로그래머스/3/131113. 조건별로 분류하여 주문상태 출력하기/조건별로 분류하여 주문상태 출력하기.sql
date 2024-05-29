08d49fb443a8ede3d706831d7418a8abeb1a4976
Unho Yeo <yuh001205@gmail.com>
2024-05-29 20:46:56 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,7 @@
+select ORDER_ID, PRODUCT_ID, date_format(OUT_DATE, '%Y-%m-%d'),
+    case when OUT_DATE <= '2022-05-01' then '출고완료'
+         when OUT_DATE > '2022-05-01' then '출고대기'
+         else '출고미정'
+    end as '출고여부'
+from FOOD_ORDER
+order by 1
