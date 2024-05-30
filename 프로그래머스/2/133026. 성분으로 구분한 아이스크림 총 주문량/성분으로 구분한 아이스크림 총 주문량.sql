95b61950077b09152a435a135ad0dbd0350dc77a
Unho Yeo <yuh001205@gmail.com>
2024-05-30 18:22:08 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -1,4 +1,4 @@
-select INGREDIENT_TYPE, sum(TOTAL_ORDER)
+select INGREDIENT_TYPE, sum(TOTAL_ORDER) as TOTAL_ORDER
 from FIRST_HALF f join ICECREAM_INFO i on f.FLAVOR = i.FLAVOR
 group by 1
 order by 2
