701126d3cefa14edce0c45c4290e751a44f12338
Unho Yeo <yuh001205@gmail.com>
2024-06-09 10:24:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -4,6 +4,9 @@ def solution(s):
     n = ['zero', 'one', 'two', 'three', 'four', 
          'five', 'six', 'seven', 'eight', 'nine']
     
+    if s.isdigit():
+        return int(s)
+    
     for c in s:
         if c.isdigit():
             result += c
