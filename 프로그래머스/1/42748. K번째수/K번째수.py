cd693b14c5e706ffe6c6aa182e56816142029159
Unho Yeo <yuh001205@gmail.com>
2024-06-12 14:42:52 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,8 @@
 def solution(array, commands):
     answer = []
     
-    for c in commands:
-        answer.append(sorted(array[c[0] - 1 : c[1]])[c[2] - 1])
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i-1 : j])[k-1])
         
     return answer
