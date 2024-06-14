41945d134736e2f6f25080824734eb969a5f47d8
Unho Yeo <yuh001205@gmail.com>
2024-06-14 22:46:18 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -2,4 +2,4 @@ def solution(a, b):
     dow = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     end = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    return dow[(sum(end[:a-1]) + b - 1) % 7]
+    return dow[(b + sum(end[:a-1])) % 7 - 1]
