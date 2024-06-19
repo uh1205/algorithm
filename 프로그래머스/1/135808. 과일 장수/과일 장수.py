ab0eaea5ccee5183cb2559a1147912621a2548e9
Unho Yeo <yuh001205@gmail.com>
2024-06-19 15:45:11 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,9 @@
 def solution(k, m, score):
     result = 0
     score.sort(reverse=True)
-    # 3322 111 : 0 4 : 7
     
-    for i in range(0, len(score) - m + 2, m):
-        l = score[i : i + m]
-        if len(l) == m:
-            result += l[-1] * m
+    for i in range(0, len(score) - len(score) % m, m):
+        box = score[i:i + m]
+        result += box[-1] * m
     
     return result
-    
