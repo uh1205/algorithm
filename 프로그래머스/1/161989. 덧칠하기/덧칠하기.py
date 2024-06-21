cf065eb9166baf79b02ccfd793272591cbd6b0a7
Unho Yeo <yuh001205@gmail.com>
2024-06-21 19:00:08 +0900

    [level 1] Title: 덧칠하기, Time: 1.72 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,12 +1,9 @@
 def solution(n, m, section):
-    count = 0
-    i = 0
-    
-    while i < len(section):
-        count += 1
-        current_start = section[i]
-        
-        while i < len(section) and section[i] < current_start + m:
-            i += 1
-            
-    return count
+    answer = 1
+    prev = section[0]
+    for sec in section:
+        if sec - prev >= m:
+            prev = sec
+            answer += 1
+
+    return answer
