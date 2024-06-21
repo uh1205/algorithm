486a3c1d920b2cd099ac8ceeee29c352d194ae68
Unho Yeo <yuh001205@gmail.com>
2024-06-21 19:09:58 +0900

    [level 1] Title: 덧칠하기, Time: 1.17 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,8 @@
 def solution(n, m, section):
-    answer = 0
-    # N 미터 벽 
-    # 페인트가 벗겨져 칠하기로함
-    # 룰러의 길이 M
-    #   - 구역의 일부부만 칠하기 안됨
-
-    # Greedy
-
-    coloredWall = 0
+    count = 0
+    coloredWall = 0  # 마지막으로 페인트칠한 구역의 끝 번호
     for wall in section:
         if wall > coloredWall:
-            coloredWall = wall + m -1
-            answer += 1
-    return answer
+            coloredWall = wall + m - 1  # 새로운 페인트칠한 구역의 끝 번호 업데이트
+            count += 1  # 페인트칠 횟수 증가
+    return count
