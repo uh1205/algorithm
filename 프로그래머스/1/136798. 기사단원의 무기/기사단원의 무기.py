156e23c43da9095cb3bb581044038b4d9e0a7816
Unho Yeo <yuh001205@gmail.com>
2024-06-25 15:39:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,11 +1,19 @@
-def cf(n):
-    a = []
-    for i in range(1,int(n**0.5)+1):
-        if n%i == 0:
-            a.append(n//i)
-            a.append(i)
-    return len(set(a))
-
-
 def solution(number, limit, power):
-    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
+    # 각 숫자의 약수 개수를 저장할 배열
+    divisors_count = [0] * (number + 1)
+    
+    # 배수를 이용해 약수 개수 계산
+    for i in range(1, number + 1):
+        for j in range(i, number + 1, i):
+            divisors_count[j] += 1
+    
+    # 총 필요한 철의 무게 계산
+    total_weight = 0
+    for k in range(1, number + 1):
+        if divisors_count[k] > limit:
+            total_weight += power
+        else:
+            total_weight += divisors_count[k]
+    
+    return total_weight
+
