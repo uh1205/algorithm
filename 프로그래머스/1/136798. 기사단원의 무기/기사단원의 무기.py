0465aa7fce12f545858a03c88351b0fc4154d8da
Unho Yeo <yuh001205@gmail.com>
2024-06-25 15:32:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 3.66 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,17 +1,11 @@
-def countDivisor(n):
-    count = 0
-    for i in range(1, int(n ** 0.5) + 1):
-        if n % i == 0:
-            count += 1
-            if i * i != n:
-                count += 1
-    return count
+def cf(n):
+    a = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
+
 
 def solution(number, limit, power):
-    divisors = []
-    for i in range(1, number + 1):
-        c = countDivisor(i)
-        divisors.append(power if c > limit else c)
-    return sum(divisors)
-    
-    
+    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
