65597a39161ca16011a777e1b53cdd022059ce1c
Unho Yeo <yuh001205@gmail.com>
2024-06-27 11:38:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.84 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,21 +1,15 @@
 def solution(number, limit, power):
-    # 각 숫자의 약수 개수를 저장할 배열
-    divisors_count = [0] * (number + 1)
+    count = [0] * (number + 1)
+    result = 0
     
-    # 배수를 이용해 약수 개수 계산
     for i in range(1, number + 1):
         for j in range(i, number + 1, i):
-            divisors_count[j] += 1
+            count[j] += 1
     
-    total_weight = 0
-    for k in range(1, number + 1):
-        if divisors_count[k] > limit:
-            total_weight += power
+    for i in range(1, number + 1):
+        if count[i] > limit:
+            result += power
         else:
-            total_weight += divisors_count[k]
+            result += count[i]
     
-    return total_weight
-
-# 테스트 예시
-print(solution(5, 3, 2))  # 출력: 10
-print(solution(10, 3, 2)) # 출력: 21
+    return result
