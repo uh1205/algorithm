45cbf7ac24e55f2f25cbbd9f8e589c5ac6bbd0ab
Unho Yeo <yuh001205@gmail.com>
2025-02-19 17:47:45 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 652 ms, Memory: 141760 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,22 +1,17 @@
 import java.io.*;
-import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int A;
-        int B;
-        
-        for(int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
-            bw.write(String.valueOf(A + B));
-            bw.newLine();
+        for (int i = 0; i < T; i++) {
+            String str = br.readLine();
+            int target = str.indexOf(" ");
+            int result = Integer.parseInt(str.substring(0, target))
+                    + Integer.parseInt(str.substring(target + 1));
+            sb.append(result).append("\n");
         }
-        bw.close();
+        System.out.print(sb);
     }
 }
