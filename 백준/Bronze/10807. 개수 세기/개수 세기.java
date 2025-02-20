c706b1f3cbeb68052ba477bea53bcadba1403565
Unho Yeo <yuh001205@gmail.com>
2025-02-20 13:28:13 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int a : arr) {
+            if (a == v) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
