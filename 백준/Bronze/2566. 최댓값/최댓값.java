80ef5ec572e8565507d8fb256271ffdc4ed95490
Unho Yeo <yuh001205@gmail.com>
2025-02-24 17:24:59 +0900

    [Bronze III] Title: 최댓값, Time: 68 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int row = 1; // 행
+        int col = 1; // 열
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num > max) {
+                    max = num;
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " " + col);
+    }
+}
