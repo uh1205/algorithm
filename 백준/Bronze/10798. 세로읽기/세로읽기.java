72590b1e01025ad46fa50339fb0cf8349a7daf6a
Unho Yeo <yuh001205@gmail.com>
2025-02-24 18:09:50 +0900

    [Bronze I] Title: 세로읽기, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -3,21 +3,16 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] matrix = new char[5][15];
+        String[] arr = new String[5];
         for (int i = 0; i < 5; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                matrix[i][j] = s.charAt(j);
-            }
+            arr[i] = br.readLine();
         }
         StringBuilder sb = new StringBuilder();
-        for (int j = 0; j < 15; j++) {
-            for (int i = 0; i < 5; i++) {
-                char c = matrix[i][j];
-                if (c == 0) {
-                    continue;
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j].length() > i) {
+                    sb.append(arr[j].charAt(i));
                 }
-                sb.append(c);
             }
         }
         System.out.println(sb);
