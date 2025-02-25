2168f2acfc243fe1251c326dbc973fb83a256464
Unho Yeo <yuh001205@gmail.com>
2025-02-25 19:33:49 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[100][100];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    arr[X + j][Y + k] = 1;
+                }
+            }
+        }
+        int result = 0;
+        for (int[] row : arr) {
+            for (int r : row) {
+                result += r;
+            }
+        }
+        System.out.println(result);
+    }
+}
