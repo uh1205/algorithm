e226b8864063a38f17743d0f9e4c401feb493674
Unho Yeo <yuh001205@gmail.com>
2025-03-01 21:33:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 608 ms, Memory: 125040 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        // -1 000 000 ~ 1 000 000
+        boolean[] arr = new boolean[2000001];
+
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
