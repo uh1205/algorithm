892bf66743962e149acdeb376e54b05499b6ff8a
Unho Yeo <yuh001205@gmail.com>
2025-03-03 11:31:24 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 20592 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N, M;
+    static boolean[] visited;
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        num = new int[M];
+        perm(0);
+        System.out.println(sb);
+    }
+
+    static void perm(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(num[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) { // 1 ~ N 중 M개 뽑기
+            if (visited[i]) continue;
+            visited[i] = true;
+            num[depth] = i;
+            perm(depth + 1);
+            num[depth] = 0;
+            visited[i] = false;
+        }
+    }
+}
