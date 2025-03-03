3ddd7c3d07172fda34cdf73265f4d452aa0296c5
Unho Yeo <yuh001205@gmail.com>
2025-03-03 15:42:59 +0900

    [Silver II] Title: N과 M (12), Time: 80 ms, Memory: 12500 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N, M;
+    static int[] arr, num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        num = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(num[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int lastUsed = -1; // 추가
+        for (int i = start; i < N; i++) {
+            if (arr[i] == lastUsed) continue;  // 추가
+            num[depth] = arr[i];
+            lastUsed = arr[i];  // 추가
+            dfs(depth + 1, i);
+        }
+    }
+}
