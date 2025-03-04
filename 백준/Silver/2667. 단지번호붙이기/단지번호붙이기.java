696be50ab3b33750feb76176df1aa983e7430475
Unho Yeo <yuh001205@gmail.com>
2025-03-05 00:36:16 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,62 +1,61 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
-class Main {
-
-    static int[] dr = {0, 1, 0, -1};
-    static int[] dc = {1, 0, -1, 0};
-
+public class Main {
     static int N;
-    static int[][] map;
-    static boolean[][] visited;
-
-    static List<Integer> counts = new ArrayList<>();
+    static boolean[][] map;
+    static int[] dR = {0, 0, -1, 1};
+    static int[] dC = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        map = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j) == '0' ? 0 : 1;
+                map[i][j] = (input.charAt(j) == '1');
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    visited[i][j] = true;
-                    counts.add(dfs(i, j));
+        int residence = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if(map[r][c]){
+                    residence++;
+                    pq.offer(dfs(r, c));
                 }
             }
         }
-        Collections.sort(counts);
 
         StringBuilder sb = new StringBuilder();
-        sb.append(counts.size()).append("\n");
-        counts.forEach(c -> sb.append(c).append("\n"));
-
-        System.out.println(sb);
+        sb.append(residence).append('\n');
+        while(!pq.isEmpty()){
+            sb.append(pq.poll()).append('\n');
+        }
+        System.out.print(sb);
     }
 
-    static int dfs(int r, int c) {
-        int cnt = 1;
-
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
-            if (map[nr][nc] == 1 && !visited[nr][nc]) {
-                visited[nr][nc] = true;
-                cnt += dfs(nr, nc);
+    static int dfs(int r, int c){
+        map[r][c] = false;
+        int nr, nc;
+        int houses = 1;
+        for (int dir = 0; dir < 4; dir++) {
+            nr = r + dR[dir];
+            nc = c + dC[dir];
+            if(isInBound(nr, nc)){
+                houses += dfs(nr, nc);
             }
         }
-        return cnt;
+        return houses;
+    }
+
+    static boolean isInBound(int r, int c){
+        return r >= 0 && c >= 0 && r < N && c < N && map[r][c];
     }
 
 }
