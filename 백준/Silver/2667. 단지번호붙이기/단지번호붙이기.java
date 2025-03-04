ffdabc4880d12b073d45f48df5620069079b9b57
Unho Yeo <yuh001205@gmail.com>
2025-03-05 00:41:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,61 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
+
     static int N;
     static boolean[][] map;
-    static int[] dR = {0, 0, -1, 1};
-    static int[] dC = {-1, 1, 0, 0};
+
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
         map = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
+            String s = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = (input.charAt(j) == '1');
+                map[i][j] = s.charAt(j) == '1';
             }
         }
 
-        int residence = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int cnt = 0;
+        Queue<Integer> q = new PriorityQueue<>();
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                if(map[r][c]){
-                    residence++;
-                    pq.offer(dfs(r, c));
+                if (map[r][c]) {
+                    cnt++;
+                    q.offer(dfs(r, c));
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append(residence).append('\n');
-        while(!pq.isEmpty()){
-            sb.append(pq.poll()).append('\n');
+        sb.append(cnt).append('\n');
+
+        while (!q.isEmpty()) {
+            sb.append(q.poll()).append('\n');
         }
+
         System.out.print(sb);
     }
 
-    static int dfs(int r, int c){
+    static int dfs(int r, int c) {
         map[r][c] = false;
-        int nr, nc;
-        int houses = 1;
-        for (int dir = 0; dir < 4; dir++) {
-            nr = r + dR[dir];
-            nc = c + dC[dir];
-            if(isInBound(nr, nc)){
-                houses += dfs(nr, nc);
+        int size = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+            if (map[nr][nc]) {
+                size += dfs(nr, nc);
             }
         }
-        return houses;
-    }
-
-    static boolean isInBound(int r, int c){
-        return r >= 0 && c >= 0 && r < N && c < N && map[r][c];
+        return size;
     }
 
 }
