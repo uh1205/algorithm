59104b5aeb1d6d5199bcb3e440900c2156777a89
Unho Yeo <yuh001205@gmail.com>
2025-03-05 20:02:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 68 ms, Memory: 11992 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,29 +2,22 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-    static int N, M, r, c, d, cnt = 0;
-
-    static int[][] map;
-    static boolean[][] visited;
-
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        int[][] map = new int[N][M];
 
         st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -33,56 +26,38 @@ class Main {
             }
         }
 
-        bfs(r, c);
-        System.out.println(cnt);
-    }
-
-    static void bfs(int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{r, c});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cr = cur[0];
-            int cc = cur[1];
-
-            if (map[cr][cc] == 0) {
+        int cnt = 0;
+        while (true) {
+            if (map[r][c] == 0) {
+                map[r][c] = -1;
                 cnt++;
-                map[cr][cc] = -1; // 청소됨
             }
 
-            boolean zero = false;
+            boolean flag = false;
             for (int i = 0; i < 4; i++) {
-                int nr = cr + dr[i];
-                int nc = cc + dc[i];
+                d = (d == 0) ? 3 : d - 1;
+                int nr = r + dr[d];
+                int nc = c + dc[d];
                 if (map[nr][nc] == 0) {
-                    zero = true;
+                    flag = true;
+                    r = nr;
+                    c = nc;
                     break;
                 }
             }
 
-            if (zero) {
-                while (true) {
-                    d = (d == 0) ? 3 : d - 1; // 반시계 90도 회전
-                    int nr = cr + dr[d];
-                    int nc = cc + dc[d];
-                    
-                    if (map[nr][nc] == 0) {
-                        q.add(new int[]{nr, nc});
-                        break;
-                    }
-                }
-            } else {
-                int nd = (d < 2) ? d + 2 : d - 2; // 후진
-                int nr = cr + dr[nd];
-                int nc = cc + dc[nd];
-
+            if (!flag) {
+                int nd = (d < 2) ? d + 2 : d - 2;
+                int nr = r + dr[nd];
+                int nc = c + dc[nd];
                 if (map[nr][nc] == 1) {
-                    return;
+                    break;
                 }
-                q.add(new int[]{nr, nc});
+                r = nr;
+                c = nc;
             }
         }
-    }
 
+        System.out.println(cnt);
+    }
 }
