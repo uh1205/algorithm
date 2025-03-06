2bfe82707e398929690053ede7c121bd1d42c9a0
Unho Yeo <yuh001205@gmail.com>
2025-03-06 14:16:54 +0900

    [Silver IV] Title: 동전 0, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,20 +4,22 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] coins = new int[N];
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         int cnt = 0;
         for (int i = N - 1; i >= 0; i--) {
-            if (coins[i] > K) continue;
-            cnt += K / coins[i];
-            K %= coins[i];
+            if (arr[i] <= K) {
+                cnt += K / arr[i];
+                K %= arr[i];
+            }
         }
 
         System.out.println(cnt);
