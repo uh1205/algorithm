07bb7af37d5fcdeb83276567933e71e0b7ab7630
Unho Yeo <yuh001205@gmail.com>
2025-03-06 15:20:28 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -5,13 +5,10 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
-            if (i < 4) {
-                arr[i] = i;
-            } else {
-                arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
-            }
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
         }
-        System.out.println(arr[N] % 10007);
+        System.out.println(arr[N]);
     }
 }
