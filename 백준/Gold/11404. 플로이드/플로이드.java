6e82328e67e9f472e751df640974c3eb338da64a
Unho Yeo <yuh001205@gmail.com>
2025-03-08 23:51:37 +0900

    [Gold IV] Title: 플로이드, Time: 304 ms, Memory: 41908 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,7 +4,8 @@ import java.util.*;
 class Main {
 
     static int n, m, a, b, c;
-    static int[][] dist;
+    static int[][] dist; // 최단 거리 배열
+    static final int INF = 100_000_000; // 무한대를 나타내는 값 (100,000 * 100보다 큼)
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,28 +13,31 @@ class Main {
         n = Integer.parseInt(br.readLine()); // 도시 개수
         m = Integer.parseInt(br.readLine()); // 버스 개수
 
-        dist = new int[n + 1][n + 1]; // 거리 배열
+        // 거리 배열 선언 & 초기화
+        dist = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                dist[i][j] = i == j ? 0 : 100_000_000;
-            }
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0; // 자기 자신으로 가는 비용은 0
         }
 
+        // 간선 정보 입력
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken()); // 출발 도시
+            b = Integer.parseInt(st.nextToken()); // 도착 도시
+            c = Integer.parseInt(st.nextToken()); // 비용
 
-            dist[a][b] = Math.min(dist[a][b], c); // 노선이 하나가 아닐 수 있음
+            // 같은 경로에 대해 더 작은 비용으로 갱신
+            dist[a][b] = Math.min(dist[a][b], c);
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    // j->k 가 j->i->k 보다 멀 경우
-                    if (dist[j][k] > dist[j][i] + dist[i][k]) {
-                        dist[j][k] = dist[j][i] + dist[i][k];
+        // 플로이드-워셜 알고리즘 수행
+        for (int k = 1; k <= n; k++) { // 경유지
+            for (int i = 1; i <= n; i++) { // 출발 도시
+                for (int j = 1; j <= n; j++) { // 도착 도시
+                    // k를 경유할 수 있다면
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
             }
@@ -42,11 +46,11 @@ class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                sb.append(dist[i][j] == 100_000_000 ? 0 : dist[i][j]).append(' ');
+                if (dist[i][j] == INF) sb.append("0 ");
+                else sb.append(dist[i][j]).append(' ');
             }
             sb.append('\n');
         }
-
         System.out.println(sb);
     }
 }
