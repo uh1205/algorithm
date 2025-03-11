d5724d33179d00ac98bb065c7bce9321221acb6c
Unho Yeo <yuh001205@gmail.com>
2025-03-11 22:09:29 +0900

    [Silver I] Title: 안전 영역, Time: 184 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, min = Integer.MAX_VALUE, max = 0, rain, result = 0;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                min = Math.min(min, map[i][j]);
+                max = Math.max(max, map[i][j]);
+            }
+        }
+
+        for (rain = min - 1; rain < max; rain++) {
+            int cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] > rain && !visited[i][j]) {
+                        cnt++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            result = Math.max(result, cnt);
+            visited = new boolean[N][N];
+        }
+
+        System.out.println(result);
+    }
+
+    static void dfs(int r, int c) {
+        visited[r][c] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+            if (map[nr][nc] > rain && !visited[nr][nc]) {
+                dfs(nr, nc);
+            }
+        }
+    }
+
+}
