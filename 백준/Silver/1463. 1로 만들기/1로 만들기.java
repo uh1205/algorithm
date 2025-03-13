c787ac5b22e095e14e46be6c6ae31e9452632701
Unho Yeo <yuh001205@gmail.com>
2025-03-13 16:54:26 +0900

    [Silver III] Title: 1로 만들기, Time: 84 ms, Memory: 15540 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -8,10 +8,17 @@ public class Main {
         int[] dp = new int[N + 1];
 
         for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1; // 기본적으로 1 빼는 연산
+            dp[i] = dp[i - 1] + 1; // 1을 빼고 하는 연산
 
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                // 1을 빼고 하는 연산과, 2를 나누고 하는 연산 중 적게 걸리는걸로
+            }
+            
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+                // 1을 빼고 하는 연산과, 3을 나누고 하는 연산 중 적게 걸리는걸로
+            }
         }
 
         System.out.println(dp[N]);
