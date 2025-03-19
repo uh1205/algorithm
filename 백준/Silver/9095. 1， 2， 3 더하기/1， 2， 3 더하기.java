ec92b57d4c12758891bbdead3b53fd82685da190
Unho Yeo <yuh001205@gmail.com>
2025-03-19 14:48:29 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
