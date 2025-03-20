9e070c94d7a3f9396f0bcc73da608a6ab4b95c60
Unho Yeo <yuh001205@gmail.com>
2025-03-20 17:55:36 +0900

    [Gold V] Title: AC, Time: 600 ms, Memory: 104336 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+
+        while (T-- > 0) {
+            String p = br.readLine(); // 수행할 함수
+            int n = Integer.parseInt(br.readLine()); // 배열 크기
+            String input = br.readLine(); // 배열 입력
+
+            // 배열 파싱 ([], [1,2,3] 같은 형태에서 숫자만 추출)
+            Deque<Integer> deque = new ArrayDeque<>();
+            if (n > 0) {
+                String[] numbers = input.substring(1, input.length() - 1).split(",");
+                for (String num : numbers) {
+                    deque.add(Integer.parseInt(num));
+                }
+            }
+
+            boolean reverse = false; // 뒤집힘 여부 플래그
+            boolean error = false;   // 에러 발생 여부
+
+            for (char command : p.toCharArray()) {
+                if (command == 'R') {
+                    reverse = !reverse; // 뒤집기 플래그 토글
+                } else { // 'D' 연산
+                    if (deque.isEmpty()) {
+                        error = true;
+                        break;
+                    }
+                    if (reverse) {
+                        deque.pollLast(); // 뒤에서 제거
+                    } else {
+                        deque.pollFirst(); // 앞에서 제거
+                    }
+                }
+            }
+
+            if (error) {
+                sb.append("error\n");
+            } else {
+                sb.append("[");
+                while (!deque.isEmpty()) {
+                    sb.append(reverse ? deque.pollLast() : deque.pollFirst());
+                    if (!deque.isEmpty()) sb.append(",");
+                }
+                sb.append("]\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
