d10242f8ff6bf08191109aab7e696ff2a667aa5d
Unho Yeo <yuh001205@gmail.com>
2025-03-22 21:03:21 +0900

    [Gold IV] Title: LCS 2, Time: 88 ms, Memory: 15740 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,17 +1,20 @@
 import java.io.*;
 
 public class Main {
-    static int[][] dp;
+
     static char[] A, B;
+    static int lenA, lenB;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         A = br.readLine().toCharArray();
         B = br.readLine().toCharArray();
-        int lenA = A.length, lenB = B.length;
+        lenA = A.length;
+        lenB = B.length;
         dp = new int[lenA + 1][lenB + 1];
 
-        // **1️⃣ LCS 길이 구하기 (DP)**
+        // LCS 길이 구하기 (DP)
         for (int i = 1; i <= lenA; i++) {
             for (int j = 1; j <= lenB; j++) {
                 if (A[i - 1] == B[j - 1]) {
@@ -22,15 +25,12 @@ public class Main {
             }
         }
 
-        // **출력 1: LCS 길이**
-        System.out.println(dp[lenA][lenB]);
-
-        // **2️⃣ LCS 문자열 찾기 (역추적)**
-        StringBuilder lcs = new StringBuilder();
+        // LCS 문자열 찾기 (역추적)
+        StringBuilder sb = new StringBuilder();
         int i = lenA, j = lenB;
         while (i > 0 && j > 0) {
             if (A[i - 1] == B[j - 1]) { // 공통 문자면 LCS에 추가
-                lcs.append(A[i - 1]);
+                sb.append(A[i - 1]);
                 i--;
                 j--;
             } else if (dp[i - 1][j] >= dp[i][j - 1]) { // 위쪽 값이 크다면 위로 이동
@@ -39,10 +39,8 @@ public class Main {
                 j--;
             }
         }
-
-        // **출력 2: LCS 문자열 (거꾸로 추가했으므로 뒤집어서 출력)**
-        if (lcs.length() > 0) {
-            System.out.println(lcs.reverse().toString());
-        }
+        
+        System.out.println(dp[lenA][lenB]);
+        if (sb.length() > 0) System.out.println(sb.reverse()); // 거꾸로 추가했으므로 뒤집어서 출력
     }
 }
