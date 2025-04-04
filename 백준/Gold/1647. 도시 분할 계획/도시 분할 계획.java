a995165f95150c50ecde12671d8ecf919707dca6
Unho Yeo <yuh001205@gmail.com>
2025-04-04 14:06:13 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1672 ms, Memory: 346620 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,73 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int[] parent;
-
-    static int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // 경로 압축
-        }
-        return parent[x];
-    }
-
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) parent[b] = a;
-    }
+class Main {
+    static int N, M;
+    static List<List<Edge>> graph;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); // 집 개수
-        int M = Integer.parseInt(st.nextToken()); // 길 개수
+        graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-        List<Edge> edges = new ArrayList<>();
-        parent = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) parent[i] = i;
+        visited = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(from, to, cost));
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            graph.get(A).add(new Edge(B, C));
+            graph.get(B).add(new Edge(A, C));
         }
 
-        Collections.sort(edges); // 비용 기준 오름차순 정렬
+        int sum = 0, max = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(1, 0));
 
-        int total = 0;
-        int maxEdge = 0;
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            int node = cur.node;
 
-        for (Edge e : edges) {
-            if (find(e.from) != find(e.to)) {
-                union(e.from, e.to);
-                total += e.cost;
-                maxEdge = Math.max(maxEdge, e.cost); // MST에서 가장 큰 간선 저장
+            if (visited[node]) continue;
+
+            visited[node] = true;
+            sum += cur.cost;
+            max = Math.max(max, cur.cost);
+
+            for (Edge e : graph.get(node)) {
+                if (!visited[e.node]) {
+                    pq.add(e);
+                }
             }
         }
 
-        System.out.println(total - maxEdge); // 가장 비싼 간선 제거
+        System.out.println(sum - max);
     }
 
     static class Edge implements Comparable<Edge> {
-        int from, to, cost;
+        int node, cost;
 
-        Edge(int from, int to, int cost) {
-            this.from = from;
-            this.to = to;
+        Edge(int node, int cost) {
+            this.node = node;
             this.cost = cost;
         }
 
-        @Override
         public int compareTo(Edge o) {
             return this.cost - o.cost;
         }
