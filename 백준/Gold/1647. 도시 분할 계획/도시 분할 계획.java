a2b5b1c1b55a40aeece2129cee85d70712d151ae
Unho Yeo <yuh001205@gmail.com>
2025-04-04 15:03:38 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1308 ms, Memory: 325232 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -3,50 +3,48 @@ import java.util.*;
 
 public class Main {
     static int[] parent;
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); // 집 개수
-        int M = Integer.parseInt(st.nextToken()); // 길 개수
-
-        PriorityQueue<Edge> edges = new PriorityQueue<>();
         parent = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) parent[i] = i;
+        for (int i = 1; i < N + 1; i++) parent[i] = i;
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(from, to, cost));
+            
+            pq.add(new Edge(from, to, cost));
         }
 
-//        Collections.sort(edges); // 비용 기준 오름차순 정렬
+        int total = 0, maxEdge = 0;
 
-        int total = 0;
-        int maxEdge = 0;
-
-        while (!edges.isEmpty()) {
-            Edge cur = edges.poll();
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            
             int a = find(cur.from);
             int b = find(cur.to);
+            
             if (a != b) {
                 if (a < b) parent[a] = b;
                 else parent[b] = a;
                 total += cur.cost;
-                maxEdge = Math.max(maxEdge, cur.cost); // MST에서 가장 큰 간선 저장
+                maxEdge = Math.max(maxEdge, cur.cost);
             }
         }
 
-        System.out.println(total - maxEdge); // 가장 비싼 간선 제거
+        System.out.println(total - maxEdge);
     }
 
     static int find(int x) {
         if (parent[x] != x) {
-            parent[x] = find(parent[x]); // 경로 압축
+            parent[x] = find(parent[x]);
         }
         return parent[x];
     }
@@ -60,7 +58,6 @@ public class Main {
             this.cost = cost;
         }
 
-        @Override
         public int compareTo(Edge o) {
             return this.cost - o.cost;
         }
