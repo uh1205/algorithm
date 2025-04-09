b040978fb6afc8120616a7c7189646d1eb231f48
Unho Yeo <yuh001205@gmail.com>
2025-04-09 12:43:28 +0900

    [Gold V] Title: A와 B, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -6,19 +6,24 @@ class Main {
         String S = br.readLine();
         String T = br.readLine();
 
+        StringBuilder sb = new StringBuilder(T);
+        boolean reverse = false;
+
         // 뒤에 A 추가 -> 뒤에 A 제거
         // 뒤집고 뒤에 B 추가 -> 뒤에 B 제거 후 뒤집기
-        StringBuilder sb = new StringBuilder(T);
         while (sb.length() > S.length()) {
-            int lastIdx = sb.length() - 1;
-            if (sb.charAt(lastIdx) == 'A') sb.deleteCharAt(lastIdx);
-            else if (sb.charAt(lastIdx) == 'B') {
+            int lastIdx = reverse ? 0 : sb.length() - 1;
+
+            if (sb.charAt(lastIdx) == 'A') {
                 sb.deleteCharAt(lastIdx);
-                sb.reverse();
-            }
-            else break;
+            } else if (sb.charAt(lastIdx) == 'B') {
+                sb.deleteCharAt(lastIdx);
+                reverse = !reverse;
+            } else break;
         }
 
+        if (reverse) sb.reverse();
         System.out.println(S.contentEquals(sb) ? 1 : 0);
+        // S.equals(sb.toString()) == S.contentEquals(sb)
     }
 }
