791387cc0c23e6a95bd201076be51ed4a2c829ae
Unho Yeo <yuh001205@gmail.com>
2025-04-10 16:58:16 +0900

    [Gold IV] Title: 알파벳, Time: 864 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,58 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int R, C;
-    static char[][] board;
-    static boolean[] visited = new boolean[26]; // 알파벳 방문 여부
-    static int max = 0;
-
-    // 방향 벡터 (상, 하, 좌, 우)
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+class Main {
+    static int N, M, max = 0;
+    static int[][] map;
+    static boolean[] visited = new boolean[26];
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
-        // 빠른 입력 처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        board = new char[R][C];
-
-        for (int i = 0; i < R; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = line.charAt(j);
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - 'A';
             }
         }
 
-        // 시작 알파벳 방문 체크
-        visited[board[0][0] - 'A'] = true;
+        visited[map[0][0]] = true;
         dfs(0, 0, 1);
 
         System.out.println(max);
     }
 
-    static void dfs(int x, int y, int count) {
-        max = Math.max(max, count);
+    static void dfs(int r, int c, int dist) {
+        max = Math.max(max, dist);
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-            // 보드 범위 안이고, 아직 방문하지 않은 알파벳이면 이동
-            if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
-                int index = board[nx][ny] - 'A';
-                if (!visited[index]) {
-                    visited[index] = true;
-                    dfs(nx, ny, count + 1);
-                    visited[index] = false; // 백트래킹
-                }
-            }
+            if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+
+            int idx = map[nr][nc];
+            if (visited[idx]) continue;
+
+            visited[idx] = true;
+            dfs(nr, nc, dist + 1);
+            visited[idx] = false;
         }
     }
 }
