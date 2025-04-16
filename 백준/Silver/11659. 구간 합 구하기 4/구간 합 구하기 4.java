bdab796343cdf2c625998e72850d5b8feaf4c891
Unho Yeo <yuh001205@gmail.com>
2025-04-16 17:20:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 436 ms, Memory: 56532 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,17 +8,12 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N + 1];
-        int[] prefixSum = new int[N + 1];
+        int[] arr = new int[N + 1]; // 1-based index
+        int[] prefixSum = new int[N + 1]; // 누적합 배열
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
-        }
-
-        prefixSum[0] = arr[0];
-        for (int i = 1; i < N; i++) {
             prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
 
@@ -31,6 +26,6 @@ public class Main {
             sb.append(prefixSum[j] - prefixSum[i - 1]).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
