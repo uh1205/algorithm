afbf3e067fc24ad32594b54ea1e1de4ad9d8768f
Unho Yeo <yuh001205@gmail.com>
2025-04-16 15:59:17 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22292 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -5,30 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N + 1];
-
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int left = 0, right = 0, sum = 0, minLen = Integer.MAX_VALUE;
+        int minLen = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0;
 
-        while (right <= N && left <= right) {
-            if (sum >= S) {
-                minLen = Math.min(minLen, right - left);
+        for (int right = 0; right < N; right++) {
+            sum += arr[right];
+
+            while (sum >= S) {
+                minLen = Math.min(minLen, right - left + 1);
                 sum -= arr[left];
                 left++;
-            } else {
-                sum += arr[right];
-                right++;
             }
         }
 
-        if (minLen == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(minLen);
+        System.out.println(minLen == Integer.MAX_VALUE ? 0 : minLen);
     }
 }
