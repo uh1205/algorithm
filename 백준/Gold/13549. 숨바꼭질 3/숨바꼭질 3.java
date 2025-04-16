2826c6a9d437fae46d9167f2a522b712c8097f59
Unho Yeo <yuh001205@gmail.com>
2025-04-16 15:23:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 92 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -2,44 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MAX = 100001;
+    static int MAX = 100_001; // 0 ≤ 위치 ≤ 100,000 이므로
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken()); // 수빈이 위치
         int K = Integer.parseInt(st.nextToken()); // 동생 위치
 
         int[] time = new int[MAX];
         Arrays.fill(time, -1); // 방문하지 않은 곳은 -1
 
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.add(N);
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(N);
         time[N] = 0;
 
-        while (!deque.isEmpty()) {
-            int now = deque.poll();
+        while (!dq.isEmpty()) {
+            int now = dq.poll();
 
-            if (now == K) {
-                System.out.println(time[now]);
-                return;
-            }
+            if (now == K) break; // 도착
 
-            // 순간이동: 0초
-            int next = now * 2;
+            int next;
+
+            // 순간이동 (0초)
+            next = now * 2;
             if (next < MAX && time[next] == -1) {
                 time[next] = time[now];
-                deque.addFirst(next); // 0초이므로 앞에 추가
+                dq.addFirst(next); // 0초이므로 앞에 추가
             }
 
-            // 걷기: 1초
-            for (int dir : new int[]{now - 1, now + 1}) {
-                if (dir >= 0 && dir < MAX && time[dir] == -1) {
-                    time[dir] = time[now] + 1;
-                    deque.addLast(dir); // 1초이므로 뒤에 추가
-                }
+            // -1 이동 (1초)
+            next = now - 1;
+            if (next >= 0 && time[next] == -1) {
+                time[next] = time[now] + 1;
+                dq.addLast(next); // 1초이므로 뒤에 추가
+            }
+
+            // +1 이동 (1초)
+            next = now + 1;
+            if (next < MAX && time[next] == -1) {
+                time[next] = time[now] + 1;
+                dq.addLast(next); // 1초이므로 뒤에 추가
             }
         }
+
+        System.out.println(time[K]);
     }
 }
