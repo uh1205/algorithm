7d8825bd437e906879e017fc64f8361d1714a43e
Unho Yeo <yuh001205@gmail.com>
2025-04-21 20:56:32 +0900

    [Gold III] Title: ACM Craft, Time: 668 ms, Memory: 244700 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -2,76 +2,73 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, W;
-    static int[] inDegree, time, dp;
-    static List<List<Integer>> graph, prev;
+    static int N, K, W;
+    static int[] buildTime, result;
+    static List<List<Integer>> graph;
+    static int[] indegree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 건물 개수
-            int K = Integer.parseInt(st.nextToken()); // 건설 순서 규칙 개수
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-            time = new int[N + 1]; // 건물 건설 시간
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i < N + 1; i++) {
-                time[i] = Integer.parseInt(st.nextToken());
-            }
-
+            buildTime = new int[N + 1];
+            result = new int[N + 1];
+            indegree = new int[N + 1];
             graph = new ArrayList<>();
-            prev = new ArrayList<>();
-            for (int i = 0; i < N + 1; i++) {
+            for (int i = 0; i <= N; i++) {
                 graph.add(new ArrayList<>());
-                prev.add(new ArrayList<>());
             }
 
-            inDegree = new int[N + 1]; // 해당 건물 건설 전에 먼저 건설해야 할 건물 수
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                buildTime[i] = Integer.parseInt(st.nextToken());
+                result[i] = buildTime[i]; // 기본 건설 시간
+            }
 
-            // 건설 순서: Y 건설 전에 X 먼저 건설해야 함
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int X = Integer.parseInt(st.nextToken());
-                int Y = Integer.parseInt(st.nextToken());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
 
-                graph.get(X).add(Y);
-                inDegree[Y]++; // Y 건설하기 전에 건설해야 할 건물 수 증가
+                graph.get(from).add(to);
+                indegree[to]++;
             }
 
-            W = Integer.parseInt(br.readLine()); // 건설해야 할 건물
+            W = Integer.parseInt(br.readLine());
 
-            topologicalSort();
-            sb.append(dp[W]).append('\n');
+            topologySort();
+
+            sb.append(result[W]).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void topologicalSort() {
+    static void topologySort() {
         Queue<Integer> q = new LinkedList<>();
-        dp = new int[N + 1]; // 각 건물을 건설하기까지 걸리는 시간
 
-        for (int i = 1; i < N + 1; i++) {
-            if (inDegree[i] == 0) q.add(i);
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
         }
 
         while (!q.isEmpty()) {
-            int cur = q.poll(); // 지금 건설할 건물 번호
-
-            dp[cur] = time[cur]; // 일단 자신을 건설하는 시간으로 초기화
-
-            // 자신 전에 건설해야 할 건물 중에 가장 오래 걸리는 시간을 더한 값으로 설정
-            for (int p : prev.get(cur)) {
-                dp[cur] = Math.max(dp[cur], dp[p] + time[cur]);
-            }
+            int cur = q.poll();
 
             for (int next : graph.get(cur)) {
-                prev.get(next).add(cur);
-                inDegree[next]--;
-                if (inDegree[next] == 0) q.add(next);
+                result[next] = Math.max(result[next], result[cur] + buildTime[next]);
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
             }
         }
     }
