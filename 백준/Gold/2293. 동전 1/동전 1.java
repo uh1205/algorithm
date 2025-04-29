b1a9a634a5ca709133a675f747a2c7ccf49327db
Unho Yeo <yuh001205@gmail.com>
2025-04-29 19:06:45 +0900

    [Gold IV] Title: 동전 1, Time: 72 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -5,23 +5,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 동전 종류 수
-        int k = Integer.parseInt(st.nextToken()); // 목표 금액
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine()); // 각 동전의 가치
+        int[] coins = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[k + 1]; // dp[i]: i원을 만드는 경우의 수
-        dp[0] = 1; // 0원을 만드는 방법은 아무 동전도 사용하지 않는 경우 1개
+        int[] dp = new int[K + 1]; // dp[i] = i원을 만드는 경우의 수
+
+        dp[0] = 1; // 아무 것도 고르지 않는 경우
 
         for (int coin : coins) {
-            for (int i = coin; i <= k; i++) {
-                dp[i] += dp[i - coin];
+            for (int i = coin; i <= K; i++) {
+                dp[i] += dp[i - coin]; // [i-coin]원을 만드는 경우에서 [coin]원을 하나씩 추가
             }
         }
 
-        System.out.println(dp[k]);
+        System.out.println(dp[K]);
     }
 }
