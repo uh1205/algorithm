567eb8c996a0956e06189730523632c0dac5111c
Unho Yeo <yuh001205@gmail.com>
2025-04-30 17:14:04 +0900

    [Gold V] Title: 전깃줄, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -2,51 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int MAX = 501;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        Line[] lines = new Line[N];
+
+        int[] cables = new int[MAX];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            lines[i] = new Line(a, b);
+            cables[a] = b;
         }
 
-        Arrays.sort(lines); // A 기준 정렬
+        int[] dp = new int[MAX];
 
-        int[] dp = new int[N]; // LIS 저장용
-        Arrays.fill(dp, 1);
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (lines[i].b > lines[j].b) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        for (int i = 1; i < MAX; i++) {
+            if (cables[i] != 0) {
+                dp[i] = 1;
+                for (int j = 1; j < i; j++) {
+                    if (cables[j] != 0 && cables[j] < cables[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
                 }
             }
         }
 
-        int maxLIS = 0;
-        for (int val : dp) {
-            maxLIS = Math.max(maxLIS, val);
+        int max = 1;
+
+        for (int i = 1; i < MAX; i++) {
+            max = Math.max(max, dp[i]);
         }
 
-        System.out.println(N - maxLIS); // 전체 - LIS 길이 = 제거할 개수
-    }
-
-    static class Line implements Comparable<Line> {
-        int a, b;
-
-        Line(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        @Override
-        public int compareTo(Line other) {
-            return this.a - other.a; // A 기준으로 정렬
-        }
+        System.out.println(N - max);
     }
 }
