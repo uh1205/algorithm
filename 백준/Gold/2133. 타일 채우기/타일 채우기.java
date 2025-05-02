19bc166c7f6a23bd06063f78f4c7bc388ba793aa
Unho Yeo <yuh001205@gmail.com>
2025-05-02 16:41:46 +0900

    [Gold IV] Title: 타일 채우기, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 2 == 1) {
+            System.out.println(0); // 홀수면 채울 수 없음
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        dp[2] = 3;
+
+        for (int i = 4; i <= N; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
