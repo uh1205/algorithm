d1b604b05cde7564af670debf14ab86d354e01be
Unho Yeo <yuh001205@gmail.com>
2025-05-21 23:47:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 248 ms, Memory: 27268 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = br.readLine();
+        String bomb = br.readLine();
+        int bombLen = bomb.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            sb.append(input.charAt(i));
+
+            // 현재 문자열의 끝부분이 폭발 문자열과 같으면 제거
+            if (sb.length() >= bombLen) {
+                boolean isBoom = true;
+                for (int j = 0; j < bombLen; j++) {
+                    if (sb.charAt(sb.length() - bombLen + j) != bomb.charAt(j)) {
+                        isBoom = false;
+                        break;
+                    }
+                }
+
+                if (isBoom) {
+                    sb.delete(sb.length() - bombLen, sb.length());
+                }
+            }
+        }
+
+        System.out.println(sb.length() == 0 ? "FRULA" : sb.toString());
+    }
+}
