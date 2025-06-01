c87adca08ececcc10ed1a35eedfdb8acf30b1a47
Unho Yeo <yuh001205@gmail.com>
2025-06-01 21:00:52 +0900

    [Gold V] Title: 탑, Time: 656 ms, Memory: 115896 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -7,41 +7,32 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         Stack<Tower> stack = new Stack<>();
-        int[] result = new int[N];
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int height = Integer.parseInt(st.nextToken());
 
-            // 현재 탑보다 낮은 탑은 레이저를 수신할 수 없음 → pop
+            // 현재 탑보다 낮은 탑 모두 제거
             while (!stack.isEmpty() && stack.peek().height < height) {
                 stack.pop();
             }
-
-            // 스택이 비지 않았다면 수신 가능한 탑이 존재
-            if (!stack.isEmpty()) {
-                result[i] = stack.peek().index;
-            } else {
-                result[i] = 0;
-            }
-
-            // 현재 탑을 스택에 push
-            stack.push(new Tower(i + 1, height)); // 인덱스는 1-based
+            
+            if (stack.isEmpty()) sb.append(0);
+            else sb.append(stack.peek().number);
+            sb.append(' ');
+            
+            stack.push(new Tower(i + 1, height));
         }
 
-        // 결과 출력
-        StringBuilder sb = new StringBuilder();
-        for (int num : result) {
-            sb.append(num).append(" ");
-        }
         System.out.println(sb);
     }
 
     static class Tower {
-        int index, height;
+        int number, height;
 
-        public Tower(int index, int height) {
-            this.index = index;
+        Tower(int number, int height) {
+            this.number = number;
             this.height = height;
         }
     }
