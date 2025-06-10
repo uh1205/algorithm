bd294e6396daba4cd109dc1b7b42d41cb3b2dbfc
Unho Yeo <yuh001205@gmail.com>
2025-06-10 11:31:00 +0900

    [Gold II] Title: 가운데를 말해요, Time: 332 ms, Memory: 34960 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -4,23 +4,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
 
-        List<Integer> numbers = new ArrayList<>();
+        // 중간값 이하 값을 저장하는 최대 힙 (가장 큰 값이 peek)
+        PriorityQueue<Integer> leftHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        StringBuilder sb = new StringBuilder();
+        // 중간값 초과 값을 저장하는 최소 힙 (가장 작은 값이 peek)
+        PriorityQueue<Integer> rightHeap = new PriorityQueue<>();
+
         for (int i = 0; i < N; i++) {
-            int input = Integer.parseInt(br.readLine());
-            int idx = Collections.binarySearch(numbers, input);
-            if (idx < 0) idx = -idx - 1;
-            numbers.add(idx, input);
-            int size = numbers.size();
-            int mid = size % 2 == 0
-                    ? size / 2 - 1
-                    : size / 2;
-            sb.append(numbers.get(mid)).append('\n');
+            int num = Integer.parseInt(br.readLine());
+
+            // leftHeap이 비어있거나, num이 중간값 이하일 경우 leftHeap에 삽입
+            if (leftHeap.isEmpty() || num <= leftHeap.peek()) {
+                leftHeap.add(num);
+            } else {
+                rightHeap.add(num);
+            }
+
+            // 힙 크기 균형 조정
+            if (leftHeap.size() > rightHeap.size() + 1) {
+                rightHeap.add(leftHeap.poll());
+            } else if (rightHeap.size() > leftHeap.size()) {
+                leftHeap.add(rightHeap.poll());
+            }
+
+            // 현재까지의 중간값은 항상 leftHeap의 루트
+            sb.append(leftHeap.peek()).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
