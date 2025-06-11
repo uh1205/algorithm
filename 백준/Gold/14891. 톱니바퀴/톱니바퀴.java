4e03f94d0e4e20bdf219581a107e0d9eeda86213
Unho Yeo <yuh001205@gmail.com>
2025-06-11 13:54:16 +0900

    [Gold V] Title: 톱니바퀴, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -2,94 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[][] gears = new char[5][8];
+    static char[][] gears = new char[4][8];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             gears[i] = br.readLine().toCharArray();
         }
 
-        int K = Integer.parseInt(br.readLine()); // 회전 횟수
+        int K = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < K; i++) {
-            char[][] copiedGears = copyGears();
-
+        while (K-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int gearNum = Integer.parseInt(st.nextToken());
-            int direction = Integer.parseInt(st.nextToken()); // 1이면 시계, -1이면 반시계 방향
-            rotateGear(copiedGears[gearNum], direction);
+            int gearIdx = Integer.parseInt(st.nextToken()) - 1;
+            int direction = Integer.parseInt(st.nextToken());
 
-            int cur = gearNum;
-            int curDir = direction;
-            int left = gearNum - 1;
-            while (left >= 1) {
-                if (gears[cur][6] != gears[left][2]) {
-                    rotateGear(copiedGears[left], -curDir);
-                    cur = left;
-                    curDir = -curDir;
-                    left--;
+            int[] directions = new int[4];
+            directions[gearIdx] = direction;
+
+            for (int left = gearIdx - 1; left >= 0; left--) {
+                if (gears[left][2] != gears[left + 1][6]) {
+                    directions[left] = -directions[left + 1];
                 } else {
                     break;
                 }
             }
 
-            cur = gearNum;
-            curDir = direction;
-            int right = gearNum + 1;
-            while (right <= 4) {
-                if (gears[cur][2] != gears[right][6]) {
-                    rotateGear(copiedGears[right], -curDir);
-                    cur = right;
-                    curDir = -curDir;
-                    right++;
+            for (int right = gearIdx + 1; right <= 3; right++) {
+                if (gears[right - 1][2] != gears[right][6]) {
+                    directions[right] = -directions[right - 1];
                 } else {
                     break;
                 }
             }
 
-            gears = copiedGears;
+            for (int i = 0; i < 4; i++) {
+                if (directions[i] == 1) rotateRight(i);
+                else if (directions[i] == -1) rotateLeft(i);
+            }
         }
 
-        int total = 0;
-        for (int i = 1; i <= 4; i++) {
+        int score = 0;
+        for (int i = 0; i < 4; i++) {
             if (gears[i][0] == '1') {
-                total += (int) Math.pow(2, i - 1);
+                score += (1 << i);
             }
         }
 
-        System.out.println(total);
+        System.out.println(score);
     }
 
-    static char[][] copyGears() {
-        char[][] copied = new char[5][8];
-        for (int i = 0; i < copied.length; i++) {
-            copied[i] = gears[i].clone();
-        }
-        return copied;
-    }
-
-    static void rotateGear(char[] arr, int direction) {
-        if (direction == 1) rotateRight(arr);
-        else rotateLeft(arr);
-    }
-
-    // 시계 방향 회전: 오른쪽으로 한 칸 이동
-    static void rotateRight(char[] arr) {
-        char last = arr[7];
+    static void rotateRight(int gearIdx) {
+        char last = gears[gearIdx][7];
         for (int i = 7; i >= 1; i--) {
-            arr[i] = arr[i - 1];
+            gears[gearIdx][i] = gears[gearIdx][i - 1];
         }
-        arr[0] = last;
+        gears[gearIdx][0] = last;
     }
 
-    // 반시계 방향 회전: 왼쪽으로 한 칸 이동
-    static void rotateLeft(char[] arr) {
-        char first = arr[0];
+    static void rotateLeft(int gearIdx) {
+        char first = gears[gearIdx][0];
         for (int i = 0; i <= 6; i++) {
-            arr[i] = arr[i + 1];
+            gears[gearIdx][i] = gears[gearIdx][i + 1];
         }
-        arr[7] = first;
+        gears[gearIdx][7] = first;
     }
 }
