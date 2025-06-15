0e8567729a73621fba3224d31517583e845dceaa
Unho Yeo <yuh001205@gmail.com>
2025-06-15 21:11:31 +0900

    [Gold I] Title: 2048 (Easy), Time: 256 ms, Memory: 46352 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -2,14 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N;
     static int maxBlock = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
+        
         int[][] board = new int[N][N];
 
         for (int i = 0; i < N; i++) {
