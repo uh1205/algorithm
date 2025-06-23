61b58d5a24fb0ac4b72addd547a435e0f447b0a8
Unho Yeo <yuh001205@gmail.com>
2025-06-23 21:40:35 +0900

    [Gold V] Title: 입국심사, Time: 232 ms, Memory: 21956 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -12,16 +12,14 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Long.parseLong(st.nextToken());
-
         times = new long[N];
-        long maxTime = 0;
 
+        long maxTime = 0;
         for (int i = 0; i < N; i++) {
             times[i] = Long.parseLong(br.readLine());
             maxTime = Math.max(maxTime, times[i]);
         }
 
-        // 이분 탐색 범위: 최소 1 ~ 최대 maxTime * M
         long left = 1;
         long right = maxTime * M;
         long answer = right;
@@ -30,21 +28,22 @@ public class Main {
             long mid = (left + right) / 2;
 
             if (canProcessAll(mid)) {
-                answer = mid;      // 가능한 시간 중 최소값을 찾아야 하므로 저장하고 왼쪽 탐색
-                right = mid - 1;
+                answer = mid;
+                right = mid - 1; // 더 빠른 시간 탐색
             } else {
-                left = mid + 1;
+                left = mid + 1; // 더 많은 시간 필요
             }
         }
 
         System.out.println(answer);
     }
 
-    static boolean canProcessAll(long timeLimit) {
+    // mid 시간 안에 M명 이상 심사 가능한지 판단
+    static boolean canProcessAll(long mid) {
         long people = 0;
         for (int i = 0; i < N; i++) {
-            people += timeLimit / times[i];
-            if (people >= M) return true; // 조기 탈출 가능
+            people += mid / times[i];
+            if (people >= M) return true; // 조기 종료
         }
         return false;
     }
