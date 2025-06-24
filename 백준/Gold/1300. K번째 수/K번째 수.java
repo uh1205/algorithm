d6c61c3146a97fcc158d46da8f61b908cd35e85e
Unho Yeo <yuh001205@gmail.com>
2025-06-24 21:16:49 +0900

    [Gold I] Title: K번째 수, Time: 104 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+    static long K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        K = Long.parseLong(br.readLine());
+
+        long left = 1;
+        long right = (long) N * N;
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (countLessOrEqual(mid) >= K) {
+                answer = mid;
+                right = mid - 1; // 더 작은 값도 가능한지 탐색
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    // A[i][j] <= x인 수의 개수 세기
+    static long countLessOrEqual(long x) {
+        long count = 0;
+        for (int i = 1; i <= N; i++) {
+            count += Math.min(N, x / i);
+        }
+        return count;
+    }
+}
