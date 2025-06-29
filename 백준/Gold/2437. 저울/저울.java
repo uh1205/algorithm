ae6172d558f25f3a473b67ce4cae0bd42e4f6e67
Unho Yeo <yuh001205@gmail.com>
2025-06-29 22:31:50 +0900

    [Gold II] Title: 저울, Time: 76 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] weights = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            weights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(weights);
+
+        long target = 1; // 만들 수 없는 최소 무게
+
+        for (int w : weights) {
+            if (w > target) break;
+            target += w;
+        }
+
+        System.out.println(target);
+    }
+}
