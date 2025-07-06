2c490ddda3370d237d79ee359404606bcef35f1b
Unho Yeo <yuh001205@gmail.com>
2025-07-06 12:24:35 +0900

    [Gold IV] Title: 팰린드롬?, Time: 644 ms, Memory: 231304 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,56 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int[][] dp;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(br.readLine());
-       
-        arr = new int[N + 1];
-        dp = new int[N + 1][N + 1];
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        boolean[][] dp = new boolean[N + 1][N + 1]; // dp[i][j] = i ~ j 구간이 팰린드롬?
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        preprocess();
-
-        int M = Integer.parseInt(br.readLine());
-        for (int m = 0; m < M; m++) {
-            st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-
-            sb.append(dp[S][E]).append('\n');
-        }
-
-        System.out.print(sb);
-    }
-
-    static void preprocess() {
-        // 길이 1
+        // 전처리: 길이 1
         for (int i = 1; i <= N; i++) {
-            dp[i][i] = 1;
+            dp[i][i] = true;
         }
 
-        // 길이 2
+        // 전처리: 길이 2
         for (int i = 1; i < N; i++) {
-            if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1;
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
         }
 
-        // 길이 3 이상
+        // 전처리: 길이 3 이상
         for (int len = 3; len <= N; len++) {
-            for (int i = 1; i <= N - len + 1; i++) {
-                int j = i + len - 1;
-                if (arr[i] == arr[j] && dp[i + 1][j - 1] == 1) {
-                    dp[i][j] = 1;
+            for (int s = 1; s <= N - len + 1; s++) {
+                int e = s + len - 1;
+                if (arr[s] == arr[e] && dp[s + 1][e - 1]) {
+                    dp[s][e] = true;
                 }
             }
         }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        // 질의 시작
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(dp[s][e] ? 1 : 0).append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
