4317a5e488ede81fa04e745e62ac383b18a8adc3
Unho Yeo <yuh001205@gmail.com>
2025-07-10 17:51:08 +0900

    [Gold II] Title: 퍼즐, Time: 472 ms, Memory: 79044 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -2,64 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final String goal = "123456780";
-    static final int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
+    static final String GOAL = "123456780";
+    static int[] dr = {1, -1, 0, 0};
+    static int[] dc = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder start = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens()) {
-                start.append(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                sb.append(st.nextToken());
             }
         }
 
-        System.out.println(bfs(start.toString()));
+        System.out.println(bfs(sb.toString()));
     }
 
-    static int bfs(String start) {
+    static int bfs(String map) {
         Queue<String> q = new LinkedList<>();
-        Map<String, Integer> dist = new HashMap<>();
+        Set<String> visited = new HashSet<>();
 
-        q.offer(start);
-        dist.put(start, 0);
+        q.add(map);
+        visited.add(map);
+
+        int depth = 0;
 
         while (!q.isEmpty()) {
-            String cur = q.poll();
-            int d = dist.get(cur);
+            int size = q.size();
+            
+            while (size-- > 0) {
+                String cur = q.poll();
 
-            if (cur.equals(goal)) return d;
+                if (cur.equals(GOAL)) {
+                    return depth;
+                }
 
-            int zero = cur.indexOf('0');
-            int x = zero / 3;
-            int y = zero % 3;
+                int zeroIdx = cur.indexOf("0");
+                int cr = zeroIdx / 3;
+                int cc = zeroIdx % 3;
 
-            for (int[] dir : dirs) {
-                int nx = x + dir[0];
-                int ny = y + dir[1];
+                for (int d = 0; d < 4; d++) {
+                    int nr = cr + dr[d];
+                    int nc = cc + dc[d];
 
-                if (nx < 0 || ny < 0 || nx >= 3 || ny >= 3) continue;
+                    if (nr < 0 || nc < 0 || nr >= 3 || nc >= 3) continue;
 
-                int swapIdx = nx * 3 + ny;
+                    int swapIdx = nr * 3 + nc;
+                    String next = swap(cur, zeroIdx, swapIdx);
 
-                String next = swap(cur, zero, swapIdx);
-                if (!dist.containsKey(next)) {
-                    dist.put(next, d + 1);
-                    q.offer(next);
+                    if (!visited.contains(next)) {
+                        q.add(next);
+                        visited.add(next);
+                    }
                 }
             }
+            
+            depth++;
         }
 
-        return -1; // 목표 상태 도달 불가
+        return -1;
     }
 
-    static String swap(String s, int i, int j) {
-        char[] arr = s.toCharArray();
-        char tmp = arr[i];
+    static String swap(String str, int i, int j) {
+        char[] arr = str.toCharArray();
+        char temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = tmp;
+        arr[j] = temp;
         return new String(arr);
     }
 }
