85f74a724034f0c3fc6dfbb79fb00371c1dc8534
Unho Yeo <yuh001205@gmail.com>
2025-07-13 11:51:02 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,65 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Node[] tree = new Node[26]; // A~Z 노드
+    static Node[] tree;
+    static StringBuilder pre = new StringBuilder();
+    static StringBuilder in = new StringBuilder();
+    static StringBuilder post = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        // 노드 초기화
-        for (int i = 0; i < 26; i++) {
+        tree = new Node[26];
+
+        for (int i = 0; i < tree.length; i++) {
             tree[i] = new Node();
         }
 
-        // 트리 입력 구성
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char parent = st.nextToken().charAt(0);
+            char node = st.nextToken().charAt(0);
             char left = st.nextToken().charAt(0);
             char right = st.nextToken().charAt(0);
 
-            int idx = parent - 'A';
-            tree[idx].left = (left == '.') ? -1 : left - 'A';
-            tree[idx].right = (right == '.') ? -1 : right - 'A';
+            tree[node - 'A'].left = left;
+            tree[node - 'A'].right = right;
         }
 
-        StringBuilder pre = new StringBuilder();
-        StringBuilder in = new StringBuilder();
-        StringBuilder post = new StringBuilder();
-
-        preorder(0, pre); // 루트는 항상 A (인덱스 0)
-        inorder(0, in);
-        postorder(0, post);
+        preorder('A');
+        inorder('A');
+        postorder('A');
 
         System.out.println(pre);
         System.out.println(in);
         System.out.println(post);
     }
 
-    static void preorder(int node, StringBuilder sb) {
-        if (node == -1) return;
-        sb.append((char) (node + 'A'));
-        preorder(tree[node].left, sb);
-        preorder(tree[node].right, sb);
+    static void preorder(char node) {
+        if (node == '.') return;
+        pre.append(node);
+        preorder(tree[node - 'A'].left);
+        preorder(tree[node - 'A'].right);
     }
 
-    static void inorder(int node, StringBuilder sb) {
-        if (node == -1) return;
-        inorder(tree[node].left, sb);
-        sb.append((char) (node + 'A'));
-        inorder(tree[node].right, sb);
+    static void inorder(char node) {
+        if (node == '.') return;
+        inorder(tree[node - 'A'].left);
+        in.append(node);
+        inorder(tree[node - 'A'].right);
     }
 
-    static void postorder(int node, StringBuilder sb) {
-        if (node == -1) return;
-        postorder(tree[node].left, sb);
-        postorder(tree[node].right, sb);
-        sb.append((char) (node + 'A'));
+    static void postorder(char node) {
+        if (node == '.') return;
+        postorder(tree[node - 'A'].left);
+        postorder(tree[node - 'A'].right);
+        post.append(node);
     }
 
     static class Node {
-        int left = -1;
-        int right = -1;
+        char left;
+        char right;
+
+        Node() {
+            this.left = '.';
+            this.right = '.';
+        }
     }
 }
