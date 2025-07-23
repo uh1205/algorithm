01f8b26743db2c8594eab967ba9de60b84dfe52b
Unho Yeo <yuh001205@gmail.com>
2025-07-23 15:48:54 +0900

    [Gold IV] Title: 알고스팟, Time: 76 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main {
     static final int INF = Integer.MAX_VALUE;
     static int R, C;
-    static int[][] map, dist;
+    static int[][] map;
+    static boolean[][] visited;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
@@ -15,62 +16,60 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
 
         map = new int[R][C];
-        dist = new int[R][C];
+        visited = new boolean[R][C];
 
         for (int i = 0; i < R; i++) {
             String line = br.readLine();
             for (int j = 0; j < C; j++) {
                 map[i][j] = line.charAt(j) - '0';
-                dist[i][j] = INF;
             }
         }
 
-        dijkstra();
-
-        System.out.println(dist[R - 1][C - 1]);
+        System.out.println(bfs());
     }
 
-    static void dijkstra() {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(0, 0, 0));
-        dist[0][0] = 0;
+    static int bfs() {
+        Deque<Edge> dq = new ArrayDeque<>();
+        dq.add(new Edge(0, 0, 0));
+        visited[0][0] = true;
 
-        while (!pq.isEmpty()) {
-            Edge cur = pq.poll();
+        while (!dq.isEmpty()) {
+            Edge cur = dq.poll();
             int cr = cur.r;
             int cc = cur.c;
             int cw = cur.w;
 
-            if (cw > dist[cr][cc]) continue;
+            if (cr == R - 1 && cc == C - 1) {
+                return cw;
+            }
 
             for (int d = 0; d < 4; d++) {
                 int nr = cr + dr[d];
                 int nc = cc + dc[d];
 
                 if (nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
+                if (visited[nr][nc]) continue;
 
-                int nw = cw + map[nr][nc];
+                visited[nr][nc] = true;
 
-                if (nw < dist[nr][nc]) {
-                    dist[nr][nc] = nw;
-                    pq.add(new Edge(nr, nc, nw));
+                if (map[nr][nc] == 0) {
+                    dq.addFirst(new Edge(nr, nc, cw));
+                } else {
+                    dq.addLast(new Edge(nr, nc, cw + 1));
                 }
             }
         }
+
+        return -1;
     }
 
-    static class Edge implements Comparable<Edge> {
+    static class Edge {
         int r, c, w;
 
         public Edge(int r, int c, int w) {
             this.r = r;
             this.c = c;
             this.w = w;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.w - o.w;
         }
     }
 }
