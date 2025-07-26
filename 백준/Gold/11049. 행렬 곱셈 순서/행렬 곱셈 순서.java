914e4673307fcee7948391d714f3509631962850
Unho Yeo <yuh001205@gmail.com>
2025-07-26 17:31:16 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 196 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] matrix = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // dp[i][j] = i번째 행렬부터 j번째 행렬까지 곱할 때 필요한 최소 연산 횟수
+        int[][] dp = new int[N][N];
+
+        for (int len = 1; len < N; len++) {
+            for (int i = 0; i + len < N; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k + 1][j]
+                            + matrix[i][0] * matrix[k][1] * matrix[j][1];
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+
+        System.out.println(dp[0][N - 1]);
+    }
+}
