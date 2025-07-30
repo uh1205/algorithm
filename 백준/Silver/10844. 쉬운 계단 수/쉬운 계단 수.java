f1527e47af70340894e3145376206dda1c9189e3
Unho Yeo <yuh001205@gmail.com>
2025-07-30 12:52:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // dp[i][j]: 길이 i이고 마지막 숫자가 j인 계단 수의 개수
+        int[][] dp = new int[N + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j > 0) dp[i][j] += dp[i - 1][j - 1];
+                if (j < 9) dp[i][j] += dp[i - 1][j + 1];
+                dp[i][j] %= MOD;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result = (result + dp[N][i]) % MOD;
+        }
+
+        System.out.println(result);
+    }
+}
