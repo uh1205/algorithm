9352daa357f10a9da5fd7b32080c1e687795ab84
Unho Yeo <yuh001205@gmail.com>
2025-08-04 13:20:54 +0900

    [Silver I] Title: 포도주 시식, Time: 92 ms, Memory: 13364 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,11 +1,9 @@
 import java.io.*;
 
 public class Main {
-    static int n;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n + 1];
 
@@ -13,25 +11,18 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(solve(arr));
-    }
-
-    static int solve(int[] arr) {
-        if (n == 1) return arr[1];
-        if (n == 2) return arr[1] + arr[2];
-
         int[] dp = new int[n + 1];
 
         dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
+        if (n >= 2) dp[2] = arr[1] + arr[2];
 
         for (int i = 3; i <= n; i++) {
-            int a = dp[i - 1];
+            int a = arr[i] + arr[i - 1] + dp[i - 3];
             int b = arr[i] + dp[i - 2];
-            int c = arr[i] + arr[i - 1] + dp[i - 3];
+            int c = dp[i - 1];
             dp[i] = Math.max(a, Math.max(b, c));
         }
 
-        return dp[n];
+        System.out.println(dp[n]);
     }
 }
