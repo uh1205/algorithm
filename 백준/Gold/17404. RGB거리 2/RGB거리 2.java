209f87a7d35017e7db392587145854f0ac6431b6
Unho Yeo <yuh001205@gmail.com>
2025-08-13 15:08:29 +0900

    [Gold IV] Title: RGB거리 2, Time: 76 ms, Memory: 12180 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int INF = Integer.MAX_VALUE;
+    static final int INF = 1_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,30 +17,32 @@ public class Main {
             }
         }
 
-        int[][][] dp = new int[N][3][3];
+        int answer = INF;
 
-        for (int i = 0; i < 3; i++) {
-            Arrays.fill(dp[0][i], INF);
-            dp[0][i][i] = cost[0][i];
-        }
+        // 첫 번째 집 색상 고정
+        for (int first = 0; first < 3; first++) {
+            int[][] dp = new int[N][3];
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                int a = Math.min(dp[i - 1][1][j], dp[i - 1][2][j]);
-                dp[i][0][j] = a == INF ? INF : a + cost[i][0];
+            // 초기화
+            for (int c = 0; c < 3; c++) {
+                if (c == first) dp[0][c] = cost[0][c];
+                else dp[0][c] = INF;
+            }
 
-                int b = Math.min(dp[i - 1][0][j], dp[i - 1][2][j]);
-                dp[i][1][j] = b == INF ? INF : b + cost[i][1];
+            // DP 수행
+            for (int i = 1; i < N; i++) {
+                dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+                dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+                dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+            }
 
-                int c = Math.min(dp[i - 1][0][j], dp[i - 1][1][j]);
-                dp[i][2][j] = c == INF ? INF : c + cost[i][2];
+            // 마지막 집 색상은 첫 번째 집 색상과 달라야 함
+            for (int last = 0; last < 3; last++) {
+                if (last == first) continue;
+                answer = Math.min(answer, dp[N - 1][last]);
             }
         }
 
-        int a = Math.min(dp[N - 1][0][1], dp[N - 1][0][2]);
-        int b = Math.min(dp[N - 1][1][0], dp[N - 1][1][2]);
-        int c = Math.min(dp[N - 1][2][0], dp[N - 1][2][1]);
-
-        System.out.println(Math.min(a, Math.min(b, c)));
+        System.out.println(answer);
     }
 }
