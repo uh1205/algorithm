5ca22e90473d915d6884c53b9dc8c7468d22c9b6
Unho Yeo <yuh001205@gmail.com>
2025-09-01 19:54:09 +0900

    [Gold IV] Title: 스도쿠, Time: 368 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -2,8 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] map = new int[9][9];
-    static List<int[]> zero = new ArrayList<>();
+    static int[][] board = new int[9][9];
+    static boolean[][] rowUsed = new boolean[9][10];
+    static boolean[][] colUsed = new boolean[9][10];
+    static boolean[][] boxUsed = new boolean[9][10];
+    static List<int[]> blanks = new ArrayList<>();
     static boolean solved = false;
 
     public static void main(String[] args) throws IOException {
@@ -12,59 +15,59 @@ public class Main {
         for (int i = 0; i < 9; i++) {
             String line = br.readLine();
             for (int j = 0; j < 9; j++) {
-                map[i][j] = line.charAt(j) - '0';
-                if (map[i][j] == 0) zero.add(new int[]{i, j});
+                int num = line.charAt(j) - '0';
+                board[i][j] = num;
+                if (num == 0) {
+                    blanks.add(new int[]{i, j});
+                } else {
+                    rowUsed[i][num] = true;
+                    colUsed[j][num] = true;
+                    boxUsed[boxIndex(i, j)][num] = true;
+                }
             }
         }
 
         dfs(0);
-        printMap();
+        printBoard();
     }
 
-    static void dfs(int depth) {
-        if (depth == zero.size()) {
+    static int boxIndex(int r, int c) {
+        return (r / 3) * 3 + (c / 3);
+    }
+
+    static void dfs(int idx) {
+        if (idx == blanks.size()) {
             solved = true;
             return;
         }
 
-        int[] cur = zero.get(depth);
-        int r = cur[0];
-        int c = cur[1];
+        int[] pos = blanks.get(idx);
+        int r = pos[0];
+        int c = pos[1];
 
         for (int num = 1; num <= 9; num++) {
-            if (isValid(r, c, num)) {
-                map[r][c] = num;
-                dfs(depth + 1);
-                if (solved) return;
-                map[r][c] = 0;
-            }
+            if (rowUsed[r][num] || colUsed[c][num] ||
+                    boxUsed[boxIndex(r, c)][num]) continue;
+
+            board[r][c] = num;
+            rowUsed[r][num] = colUsed[c][num] = boxUsed[boxIndex(r, c)][num] = true;
+
+            dfs(idx + 1);
+            if (solved) return;
+
+            board[r][c] = 0;
+            rowUsed[r][num] = colUsed[c][num] = boxUsed[boxIndex(r, c)][num] = false;
         }
     }
 
-    static boolean isValid(int r, int c, int num) {
-        for (int i = 0; i < 9; i++) {
-            if (map[r][i] == num || map[i][c] == num) return false;
-        }
-        int sr = (r / 3) * 3;
-        int sc = (c / 3) * 3;
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (map[i][j] == num) return false;
-            }
-        }
-        return true;
-    }
-
-    static void printMap() {
+    static void printBoard() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                sb.append(map[i][j]);
+                sb.append(board[i][j]);
             }
             sb.append('\n');
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
-
 }
