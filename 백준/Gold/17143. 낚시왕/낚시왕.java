722a6d6287e3814993f8f449cbc932e64933dd59
Unho Yeo <yuh001205@gmail.com>
2025-09-03 16:22:46 +0900

    [Gold I] Title: 낚시왕, Time: 480 ms, Memory: 23668 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -7,23 +7,13 @@ public class Main {
     static int[] dr = {0, -1, 1, 0, 0}; // 1:위, 2:아래, 3:오른쪽, 4:왼쪽
     static int[] dc = {0, 0, 0, 1, -1};
 
-    static class Shark {
-        int r, c, s, d, z;
-        Shark(int r, int c, int s, int d, int z) {
-            this.r = r;
-            this.c = c;
-            this.s = s;
-            this.d = d;
-            this.z = z;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         map = new Shark[R + 1][C + 1];
 
         for (int i = 0; i < M; i++) {
@@ -53,39 +43,58 @@ public class Main {
                     break;
                 }
             }
-
             // 2. 상어 이동
-            Shark[][] newMap = new Shark[R + 1][C + 1];
-            for (int r = 1; r <= R; r++) {
-                for (int c = 1; c <= C; c++) {
-                    if (map[r][c] != null) {
-                        Shark sh = map[r][c];
-                        int nr = sh.r;
-                        int nc = sh.c;
-                        int d = sh.d;
-
-                        for (int s = 0; s < sh.s; s++) {
-                            if (nr + dr[d] <= 0 || nr + dr[d] > R ||
-                                nc + dc[d] <= 0 || nc + dc[d] > C) {
-                                d = (d % 2 == 0) ? d - 1 : d + 1; // 방향 반전
-                            }
-                            nr += dr[d];
-                            nc += dc[d];
-                        }
-
-                        sh.r = nr;
-                        sh.c = nc;
-                        sh.d = d;
-
-                        if (newMap[nr][nc] == null || newMap[nr][nc].z < sh.z) {
-                            newMap[nr][nc] = sh;
-                        }
-                    }
-                }
-            }
-            map = newMap; // 이동 결과 반영
+            moveSharks();
         }
 
         System.out.println(answer);
+    }
+
+    static void moveSharks() {
+        Shark[][] newMap = new Shark[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            for (int j = 1; j <= C; j++) {
+                if (map[i][j] != null) {
+                    Shark sh = map[i][j];
+                    int r = sh.r;
+                    int c = sh.c;
+                    int d = sh.d;
+                    int move = sh.s;
+
+                    while (move-- > 0) {
+                        int nr = r + dr[d];
+                        int nc = c + dc[d];
+                        if (nr < 1 || nr > R || nc < 1 || nc > C) {
+                            d = (d % 2 == 0) ? d - 1 : d + 1; // 방향 반전
+                            nr = r + dr[d]; // 반전한 방향으로 재계산
+                            nc = c + dc[d];
+                        }
+                        r = nr;
+                        c = nc;
+                    }
+
+                    sh.r = r;
+                    sh.c = c;
+                    sh.d = d;
+
+                    if (newMap[r][c] == null || newMap[r][c].z < sh.z) {
+                        newMap[r][c] = sh;
+                    }
+                }
+            }
+        }
+        map = newMap; // 이동 결과 반영
+    }
+
+    static class Shark {
+        int r, c, s, d, z;
+
+        Shark(int r, int c, int s, int d, int z) {
+            this.r = r;
+            this.c = c;
+            this.s = s;
+            this.d = d;
+            this.z = z;
+        }
     }
 }
