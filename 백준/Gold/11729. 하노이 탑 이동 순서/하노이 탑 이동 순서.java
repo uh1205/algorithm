1ff777c055deabf48e2d4d910b92d8b49d5ceba8
Unho Yeo <yuh001205@gmail.com>
2025-09-10 12:58:36 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 268 ms, Memory: 44804 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 이동 횟수 = 2^n - 1
+        sb.append((1 << N) - 1).append('\n');
+
+        // 재귀 호출 시작
+        hanoi(N, 1, 3, 2);
+
+        System.out.println(sb);
+    }
+
+    static void hanoi(int n, int from, int to, int via) {
+        if (n == 1) {
+            sb.append(from).append(' ').append(to).append('\n');
+            return;
+        }
+        // 1. n-1개를 from -> via
+        hanoi(n - 1, from, via, to);
+
+        // 2. 가장 큰 원판을 from -> to
+        sb.append(from).append(' ').append(to).append('\n');
+
+        // 3. n-1개를 via -> to
+        hanoi(n - 1, via, to, from);
+    }
+}
