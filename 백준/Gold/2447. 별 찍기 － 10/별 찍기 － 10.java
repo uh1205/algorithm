c650086a224e2ebc527129e692ef07dec49df858
Unho Yeo <yuh001205@gmail.com>
2025-09-15 19:35:32 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 204 ms, Memory: 66640 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,50 +1,48 @@
 import java.io.*;
 
 public class Main {
-    static char[][] board;
+    static char[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        board = new char[N][N];
-        draw(0, 0, N, false);
+        map = new char[N][N];
+
+        draw(N, 0, 0, false);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(board[i]).append('\n');
+        for (char[] chars : map) {
+            sb.append(chars).append('\n');
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 
-    // r, c : 시작 좌표
-    // size : 현재 정사각형 크기
-    // blank : 공백만 채워야 하는지 여부
-    static void draw(int r, int c, int size, boolean blank) {
+    static void draw(int n, int r, int c, boolean blank) {
         if (blank) {
-            // 공백 채우기
-            for (int i = r; i < r + size; i++) {
-                for (int j = c; j < c + size; j++) {
-                    board[i][j] = ' ';
+            for (int i = r; i < r + n; i++) {
+                for (int j = c; j < c + n; j++) {
+                    map[i][j] = ' ';
                 }
             }
             return;
         }
 
-        if (size == 1) {
-            board[r][c] = '*';
+        if (n == 1) {
+            map[r][c] = '*';
             return;
         }
 
-        int newSize = size / 3;
-        int cnt = 0;
-        for (int i = r; i < r + size; i += newSize) {
-            for (int j = c; j < c + size; j += newSize) {
-                cnt++;
-                if (cnt == 5) { // 가운데 블록
-                    draw(i, j, newSize, true);
+        int size = n / 3;
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                count++;
+                if (count == 5) {
+                    draw(size, r + i * size, c + j * size, true);
                 } else {
-                    draw(i, j, newSize, false);
+                    draw(size, r + i * size, c + j * size, false);
                 }
             }
         }
