af995ad80fdb1f3c5f2935d4a7b488fd44649bdb
Unho Yeo <yuh001205@gmail.com>
2025-09-21 22:50:51 +0900

    [Silver II] Title: 색종이 만들기, Time: 96 ms, Memory: 13140 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int[][] paper;
