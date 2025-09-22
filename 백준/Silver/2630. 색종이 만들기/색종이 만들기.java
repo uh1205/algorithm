c7616fd9fac358b3e3b553942d48f9886a62d85f
Unho Yeo <yuh001205@gmail.com>
2025-09-22 14:04:07 +0900

    [Silver II] Title: 색종이 만들기, Time: 88 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -2,54 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] paper;
-    static int white = 0; // 하얀색 개수
-    static int blue = 0;  // 파란색 개수
+    static char[][] map;
+    static int white = 0, blue = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        paper = new int[N][N];
+
+        map = new char[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                paper[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = st.nextToken().charAt(0);
             }
         }
 
-        divide(0, 0, N);
-
+        paper(N, 0, 0);
+        
         System.out.println(white);
         System.out.println(blue);
     }
 
-    // 영역 (x, y)에서 시작하는 size 크기 종이를 처리
-    static void divide(int x, int y, int size) {
-        if (isSameColor(x, y, size)) {
-            if (paper[x][y] == 0) {
-                white++;
-            } else {
-                blue++;
-            }
+    static void paper(int n, int r, int c) {
+        if (isAllSame(n, r, c)) {
+            if (map[r][c] == '0') white++;
+            else blue++;
             return;
         }
-
-        int newSize = size / 2;
-        divide(x, y, newSize);                 // 1사분면
-        divide(x, y + newSize, newSize);       // 2사분면
-        divide(x + newSize, y, newSize);       // 3사분면
-        divide(x + newSize, y + newSize, newSize); // 4사분면
+        int half = n / 2;
+        paper(half, r, c);
+        paper(half, r, c + half);
+        paper(half, r + half, c);
+        paper(half, r + half, c + half);
     }
 
-    // 현재 영역이 모두 같은 색인지 확인
-    static boolean isSameColor(int x, int y, int size) {
-        int color = paper[x][y];
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if (paper[i][j] != color) {
-                    return false;
-                }
+    static boolean isAllSame(int n, int r, int c) {
+        char first = map[r][c];
+        for (int i = r; i < r + n; i++) {
+            for (int j = c; j < c + n; j++) {
+                if (map[i][j] != first) return false;
             }
         }
         return true;
