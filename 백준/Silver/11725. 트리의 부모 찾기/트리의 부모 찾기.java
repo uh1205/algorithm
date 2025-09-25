2c008efe6f219278c9a1b715a7e4823c365a2916
Unho Yeo <yuh001205@gmail.com>
2025-09-25 22:31:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 448 ms, Memory: 72408 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,44 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> tree = new ArrayList<>();
+    static List<Integer>[] graph;
     static int[] parent;
-    static boolean[] visited;
+    static int N;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i <= N; i++) {
-            tree.add(new ArrayList<>());
-        }
-
+        graph = new ArrayList[N + 1];
         parent = new int[N + 1];
-        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            tree.get(a).add(b);
-            tree.get(b).add(a);
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        dfs(1);
+        dfs(1, -1);
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 2; i <= N; i++) {
             sb.append(parent[i]).append('\n');
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void dfs(int node) {
-        visited[node] = true;
-        for (int next : tree.get(node)) {
-            if (visited[next]) parent[node] = next;
-            else dfs(next);
+    static void dfs(int cur, int p) {
+        parent[cur] = p;
+        for (int next : graph[cur]) {
+            if (next != p) { // 부모로 되돌아가는 거 방지
+                dfs(next, cur);
+            }
         }
     }
 }
