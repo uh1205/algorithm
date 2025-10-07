342aec4b623a5f392df9401bf146059aefb1af36
Unho Yeo <yuh001205@gmail.com>
2025-10-07 21:41:42 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.72 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.26 ms
+메모리: 81.3 MB, 시간: 0.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 21:59:37
+2025년 10월 07일 21:41:41
 
 ### 문제 설명
 
--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,44 +1,56 @@
 class Solution {
     public String solution(String new_id) {
+        // 1. 소문자로 변환
+        String lower = new_id.toLowerCase();
+
+        // 2. 소문자, 숫자, -, _, .만 남기기
         StringBuilder sb = new StringBuilder();
-
-        // 1~3단계: 유효한 문자만 추가하면서 연속된 '.' 제거
-        boolean dotFlag = false;
-        for (char c : new_id.toCharArray()) {
-            if (c >= 'A' && c <= 'Z') { // 1단계: 대문자 -> 소문자
-                c += 32;
-            }
-
-            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_') {
-                sb.append(c);
-                dotFlag = false;
-            } else if (c == '.') {
-                if (!dotFlag && sb.length() > 0) { // 3단계: 연속된 '.' 방지 + 앞쪽 '.' 제거
-                    sb.append(c);
-                    dotFlag = true;
-                }
-            }
+        for (int i = 0; i < lower.length(); i++) {
+            char ch = lower.charAt(i);
+            if (isAllowed(ch)) sb.append(ch);
         }
 
-        // 4단계: 마지막 문자가 '.'이면 제거
+        // 3. 연속된 .을 하나로 치환
+        String str = sb.toString();
+        sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '.' && sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
+                continue;
+            }
+            sb.append(ch);
+        }
+
+        // 4. 처음이나 끝에 위치한 . 제거
+        if (sb.length() > 0 && sb.charAt(0) == '.') {
+            sb.deleteCharAt(0);
+        }
         if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
-            sb.setLength(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
 
-        // 5단계: 빈 문자열이면 "a" 추가
-        if (sb.length() == 0) sb.append("a");
+        // 5. 빈 문자열이면 a를 대입
+        if (sb.length() == 0) sb.append('a');
 
-        // 6단계: 15자 초과 시 잘라내고, 마지막 문자가 '.'이면 제거
+        // 6. 16자 이상이면 15자로 자르고, 마지막이 .이라면 제거
         if (sb.length() > 15) {
             sb.setLength(15);
-            if (sb.charAt(14) == '.') sb.setLength(14);
+            if (sb.charAt(sb.length() - 1) == '.') {
+                sb.deleteCharAt(sb.length() - 1);
+            }
         }
 
-        // 7단계: 3자 미만이면 마지막 문자 반복
-        while (sb.length() < 3) {
+        // 7. 2자 이하라면 마지막 문자를 반복
+        while (sb.length() <= 2) {
             sb.append(sb.charAt(sb.length() - 1));
         }
 
         return sb.toString();
+    }
+
+    boolean isAllowed(char ch) {
+        return (ch >= 'a' && ch <= 'z') ||
+                (ch >= '0' && ch <= '9') ||
+                (ch == '-' || ch == '_' || ch == '.');
     }
 }
