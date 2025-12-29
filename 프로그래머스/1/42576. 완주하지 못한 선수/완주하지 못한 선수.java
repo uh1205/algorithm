fd69b2e40829f372d02ae5b7fc5adcfa9d5e5838
Unho Yeo <yuh001205@gmail.com>
2025-12-29 18:40:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.15 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> participantCounts = new HashMap<>();
-        
-        for (String name : participant) {
-            participantCounts.put(name, participantCounts.getOrDefault(name, 0) + 1);
-        }
-        
-        Map<String, Integer> completionCounts = new HashMap<>();
-        
-        for (String name : completion) {
-            completionCounts.put(name, completionCounts.getOrDefault(name, 0) + 1);
-        }
-        
         String answer = "";
         
-        for (Map.Entry<String, Integer> entry : participantCounts.entrySet()) {
-            String name = entry.getKey();
-            int count = entry.getValue();
-            
-            if (!completionCounts.containsKey(name)) {
-                answer = name;
-                break;
-            }
-            
-            if (completionCounts.get(name) < count) {
-                answer = name;
+        // 1. HashMap 생성 (이름, 인원수)
+        Map<String, Integer> hm = new HashMap<>();
+        
+        // 2. 참가자 명단을 해시맵에 기록
+        for (String player : participant) {
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+        }
+        
+        // 3. 완주자 명단을 보며 인원수 차감
+        for (String player : completion) {
+            hm.put(player, hm.get(player) - 1);
+        }
+        
+        // 4. 남아있는 인원수가 0이 아닌 선수를 탐색
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer = key;
                 break;
             }
         }
