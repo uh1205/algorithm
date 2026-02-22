2488a31c6fbd9dfbec1ce93a4b582f28f16aedb6
Unho Yeo <yuh001205@gmail.com>
2026-02-22 21:48:01 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,6 +1,4 @@
-select MEMBER_ID, MEMBER_NAME, GENDER, 
-    date_format(DATE_OF_BIRTH, "%Y-%m-%d") as DATE_OF_BIRTH
+select MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d')
 from MEMBER_PROFILE
-where GENDER = 'W' and month(DATE_OF_BIRTH) = 03 and
-    TLNO is not null
-order by 1
+where month(DATE_OF_BIRTH) = 3 and GENDER = 'W' and TLNO is not null
+order by MEMBER_ID
