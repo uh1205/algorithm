19af5d25f022fb6c7dc8f91108acc17837d5468e
Unho Yeo <yuh001205@gmail.com>
2026-02-25 13:44:07 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,18 +1,24 @@
 SELECT
-    SUM(G.SCORE) AS SCORE,
-    E.EMP_NO,
-    E.EMP_NAME,
-    E.POSITION,
-    E.EMAIL
-FROM
-    HR_EMPLOYEES E
-JOIN
-    HR_GRADE G ON E.EMP_NO = G.EMP_NO
-WHERE
-    G.YEAR = 2022
-GROUP BY
-    E.EMP_NO
-ORDER BY
-    SCORE DESC
-LIMIT
-    1
+    SCORE,
+    EMP_NO,
+    EMP_NAME,
+    POSITION,
+    EMAIL
+FROM (
+    SELECT
+        RANK() OVER (ORDER BY SUM(G.SCORE) DESC) AS RK,
+        SUM(G.SCORE) AS SCORE,
+        E.EMP_NO,
+        E.EMP_NAME,
+        E.POSITION,
+        E.EMAIL
+    FROM
+        HR_EMPLOYEES E
+    JOIN
+        HR_GRADE G ON E.EMP_NO = G.EMP_NO
+    WHERE
+        G.YEAR = 2022
+    GROUP BY
+        E.EMP_NO
+) A
+WHERE RK = 1
