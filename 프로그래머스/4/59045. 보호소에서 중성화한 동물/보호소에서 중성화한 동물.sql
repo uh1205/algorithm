872a1a5d5cb43bd13cd72aeda983868797e1f583
Unho Yeo <yuh001205@gmail.com>
2026-02-25 18:50:07 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,5 +1,13 @@
-select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
-from ANIMAL_INS i join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
-where i.SEX_UPON_INTAKE like 'Intact%' 
-    and o.SEX_UPON_OUTCOME not like 'Intact%'
-order by 1
+SELECT
+    I.ANIMAL_ID,
+    I.ANIMAL_TYPE,
+    I.NAME
+FROM
+    ANIMAL_INS I
+JOIN
+    ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE
+    I.SEX_UPON_INTAKE LIKE 'Intact%'
+    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%' OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY
+    I.ANIMAL_ID
