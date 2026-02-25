c2027112db4841b577ccc46e52b77c6443ca48e5
Unho Yeo <yuh001205@gmail.com>
2026-02-25 14:25:26 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,26 +1,27 @@
+WITH AVGS AS (
+    SELECT EMP_NO, AVG(SCORE) AS SCORE_AVG
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+
 SELECT
     E.EMP_NO,
     E.EMP_NAME,
-    G.GRADE,
     CASE
-        WHEN G.GRADE = 'S' THEN E.SAL * 0.2
-        WHEN G.GRADE = 'A' THEN E.SAL * 0.15
-        WHEN G.GRADE = 'B' THEN E.SAL * 0.1
+        WHEN SCORE_AVG >= 96 THEN 'S'
+        WHEN SCORE_AVG >= 90 THEN 'A'
+        WHEN SCORE_AVG >= 80 THEN 'B'
+        ELSE 'C'
+    END AS GRADE,
+    CASE
+        WHEN SCORE_AVG >= 96 THEN E.SAL * 0.2
+        WHEN SCORE_AVG >= 90 THEN E.SAL * 0.15
+        WHEN SCORE_AVG >= 80 THEN E.SAL * 0.1
         ELSE 0
     END AS BONUS
-FROM (
-    SELECT
-        EMP_NO,
-        CASE
-            WHEN AVG(SCORE) >= 96 THEN 'S'
-            WHEN AVG(SCORE) >= 90 THEN 'A'
-            WHEN AVG(SCORE) >= 80 THEN 'B'
-            ELSE 'C'
-        END AS GRADE
-    FROM HR_GRADE
-    GROUP BY EMP_NO
-) AS G
+FROM
+    HR_EMPLOYEES E
 JOIN
-    HR_EMPLOYEES E ON G.EMP_NO = E.EMP_NO
+    AVGS A ON E.EMP_NO = A.EMP_NO
 ORDER BY
     E.EMP_NO
