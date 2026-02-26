48f736d12731055bd28071cae6df6b7d0d7f618e
Unho Yeo <yuh001205@gmail.com>
2026-02-26 19:59:35 +0900

    [level 2] Title: 가장 큰 수, Time: 169.02 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        List<String> list = new ArrayList<>();
+        int n = numbers.length;
         
-        for (int n : numbers) {
-            list.add(String.valueOf(n));
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = String.valueOf(numbers[i]);
         }
         
-        list.sort((a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(strings, (a, b) -> (b + a).compareTo(a + b));
         
-        if (list.get(0).startsWith("0")) {
+        if (strings[0].equals("0")) {
             return "0";
         }
         
         StringBuilder sb = new StringBuilder();
-        for (String s : list) {
+        for (String s : strings) {
             sb.append(s);
         }
         
