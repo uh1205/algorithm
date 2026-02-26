540ae1fbde49393b531603cfb6616344d7e1f4ed
Unho Yeo <yuh001205@gmail.com>
2026-02-26 15:46:09 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,5 +1,13 @@
-select ANIMAL_ID,
-       NAME,
-       if(SEX_UPON_INTAKE like 'Neutered%' or SEX_UPON_INTAKE like 'Spayed%', 'O', 'X') as '중성화'
-from ANIMAL_INS
-order by ANIMAL_ID
+SELECT
+    ANIMAL_ID,
+    NAME,
+    CASE
+        WHEN SEX_UPON_INTAKE LIKE 'Neutered%' 
+            OR SEX_UPON_INTAKE LIKE 'Spayed%'
+            THEN 'O'
+        ELSE 'X'
+    END AS 중성화
+FROM
+    ANIMAL_INS
+ORDER BY
+    ANIMAL_ID
