0ac4e15a203b1c4f69844945a20924fbc64d6f06
Unho Yeo <yuh001205@gmail.com>
2026-03-01 00:33:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.05 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        
-        // 배열의 원소 범위가 0~9이므로, 나올 수 없는 값으로 초기값 설정
-        int lastValue = -1;
+        Deque<Integer> dq = new ArrayDeque<>();
         
         for (int num : arr) {
-            // 현재 숫자가 바로 이전 숫자와 다를 때만 리스트에 추가
-            if (num != lastValue) {
-                list.add(num);
-                lastValue = num; // 현재 숫자를 이전 숫자로 업데이트
+            if (dq.isEmpty() || dq.peek() != num) {
+                dq.push(num);
             }
         }
-
-        // List<Integer>를 int[]로 변환
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        int n = dq.size();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = dq.pollLast();
+        }
+        
+        return ans;
     }
 }
