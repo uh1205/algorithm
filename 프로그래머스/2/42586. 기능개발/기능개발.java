8b2ac76baa0c32f4dc0ac3c43fe7b86643315a06
Unho Yeo <yuh001205@gmail.com>
2026-03-02 18:51:55 +0900

    [level 2] Title: 기능개발, Time: 3.89 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,27 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new ArrayDeque<>();
-        
-        for (int i = 0; i < progresses.length; i++) {
-            int remaining = 100 - progresses[i];
-            int days = (remaining + speeds[i] - 1) / speeds[i];
-            q.offer(days);
-        }
-        
         List<Integer> ans = new ArrayList<>();
         
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            int count = 1;
+        int day = (100 - progresses[0] + speeds[0] - 1) / speeds[0];
+        int count = 1;
+        
+        for (int i = 1; i < progresses.length; i++) {
+            int cur = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
             
-            while (!q.isEmpty() && q.peek() <= cur) {
-                q.poll();
+            if (cur <= day) {
                 count++;
+            } else {
+                ans.add(count);
+                day = cur;
+                count = 1;
             }
-            
-            ans.add(count);
         }
+        ans.add(count);
         
         return ans.stream().mapToInt(Integer::intValue).toArray();
     }
