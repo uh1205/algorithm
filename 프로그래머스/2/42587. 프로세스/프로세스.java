d44582a487c32cc80904085a61f86ef97c2274ef
Unho Yeo <yuh001205@gmail.com>
2026-03-02 20:06:00 +0900

    [level 2] Title: 프로세스, Time: 1.08 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        List<Integer> max = new ArrayList<>();
-        for (int p : priorities) {
-            max.add(p);
-        }
-        max.sort(Comparator.reverseOrder());
+        Queue<int[]> q = new ArrayDeque<>();
         
-        int i = 0;
-        int count = 0;
-        int ans = -1;
+        for (int i = 0; i < priorities.length; i++) {
+            q.offer(new int[]{priorities[i], i});
+        }
+        
+        Arrays.sort(priorities);
+        
+        int maxIdx = priorities.length - 1;
+        int ans = 0;
         
         while (true) {
-            if (priorities[i] >= max.get(count)) {
-                count++;
-                priorities[i] = 0;
-                if (i == location) {
-                    ans = count;
+            int[] cur = q.poll();
+            
+            if (cur[0] == priorities[maxIdx]) {
+                ans++;
+                maxIdx--;
+                if (cur[1] == location) {
                     break;
                 }
+            } else {
+                q.offer(cur);
             }
-            i++;
-            i %= priorities.length;
         }
         
         return ans;
