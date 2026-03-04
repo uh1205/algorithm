1d3c2ccac69221a881f2e197ae051cb76fb73b05
Unho Yeo <yuh001205@gmail.com>
2026-03-04 15:00:01 +0900

    [level 3] Title: 등굣길, Time: 0.71 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,34 +1,30 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        if (n == 1 || m == 1) return 1;
-        
         int mod = 1_000_000_007;
         
-        int[][] map = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
         
         for (int[] p : puddles) {
-            map[p[1]][p[0]] = -1;
+            dp[p[1]][p[0]] = -1;
         }
         
-        map[0][1] = 1;
+        dp[1][1] = 1;
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                // 현재 위치가 물이면 패스
-                if (map[i][j] == -1) {
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0;
                     continue;
                 }
-                map[i][j] = (map[i - 1][j] + map[i][j - 1]) % mod;
-                // 위가 물이면 +1, 왼이 물이면 +1
-                if (map[i - 1][j] == -1) {
-                    map[i][j] = (map[i][j] + 1) % mod;
+                if (i > 1) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % mod;
                 }
-                if (map[i][j - 1] == -1) {
-                    map[i][j] = (map[i][j] + 1) % mod;
+                if (j > 1) {
+                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % mod;
                 }
             }
         }
         
-        return map[n][m];
+        return dp[n][m];
     }
 }
