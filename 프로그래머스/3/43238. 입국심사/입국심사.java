98d65f6b23a002188f2c194de17d3d1d251f9765
Unho Yeo <yuh001205@gmail.com>
2026-03-04 17:30:09 +0900

    [level 3] Title: 입국심사, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,37 +1,31 @@
 class Solution {
     public long solution(int n, int[] times) {
-        int maxTime = 0;
-        for (int t : times) {
-            maxTime = Math.max(maxTime, t);
+        long maxTime = 0;
+        for (int time : times) {
+            if (time > maxTime) maxTime = time;
         }
         
         long left = 1;
-        long right = (long) maxTime * n;
+        long right = maxTime * n;
         long ans = right;
         
         while (left <= right) {
-            long mid = (left + right) / 2;
+            long mid = (left + right) >>> 1;
             
-            if (isPossible(n, times, mid)) {
+            long total = 0;
+            for (int time : times) {
+                total += mid / time;
+                if (total >= n) break;
+            }
+            
+            if (total >= n) {
                 ans = mid;
-                right = mid - 1; // 더 짧은 시간 시도
+                right = mid - 1;
             } else {
-                left = mid + 1; // 더 긴 시간 시도
+                left = mid + 1;
             }
         }
         
         return ans;
-    }
-    
-    // t초 안에 n명 모두 심사 가능?
-    boolean isPossible(int n, int[] times, long t) {
-        long total = 0;
-        for (int i = 0; i < times.length; i++) {
-            total += t / times[i];
-            if (total >= n) {
-                return true;
-            }
-        }
-        return false;
     }
 }
