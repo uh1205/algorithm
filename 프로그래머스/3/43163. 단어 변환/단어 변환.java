cf93afd990a2cf0f28f7e564da4c0e2919131f87
Unho Yeo <yuh001205@gmail.com>
2026-03-06 20:18:38 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,42 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int n = words.length;
+        boolean hasTarget = false;
+        for (String w : words) {
+            if (w.equals(target)) {
+                hasTarget = true;
+                break;
+            }
+        }
+        if (!hasTarget) return 0;
         
+        boolean[] visited = new boolean[words.length];
         Queue<String> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n];
-        
         q.offer(begin);
         
         int count = 0;
         
         while (!q.isEmpty()) {
-            count++;
             int size = q.size();
+            
             while (size-- > 0) {
                 String cur = q.poll();
+                
+                if (cur.equals(target)) {
+                    return count;
+                }
 
-                for (int i = 0; i < n; i++) {
-                    if (!visited[i] && canTransform(cur, words[i])) {
-                        if (words[i].equals(target)) {
-                            return count;
-                        }
+                for (int i = 0; i < words.length; i++) {
+                    if (!visited[i] && canConvert(cur, words[i])) {
                         visited[i] = true;
                         q.offer(words[i]);
                     }
                 }
             }
+            
+            count++;
         }
         
         return 0;
     }
     
-    boolean canTransform(String from, String to) {
+    boolean canConvert(String from, String to) {
         int diff = 0;
         for (int i = 0; i < from.length(); i++) {
             if (from.charAt(i) != to.charAt(i)) {
                 diff++;
             }
+            if (diff > 1) return false;
         }
         return diff == 1;
     }
