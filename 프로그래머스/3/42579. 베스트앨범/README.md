7c7164a508a88e30583bd7eb12d6691f5a5b69e7
Unho Yeo <yuh001205@gmail.com>
2026-03-07 11:13:26 +0900

    [level 3] Title: 베스트앨범, Time: 4.47 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 88.8 MB, 시간: 6.43 ms
+메모리: 78.8 MB, 시간: 4.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 26일 17:24:03
+2026년 03월 07일 11:13:24
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -7,26 +7,31 @@ class Solution {
         Map<String, List<Music>> genreMusic = new HashMap<>();
         
         for (int i = 0; i < genres.length; i++) {
-            genrePlayCount.put(genres[i], genrePlayCount.getOrDefault(genres[i], 0) + plays[i]);
+            genrePlayCount.put(genres[i], 
+                               genrePlayCount.getOrDefault(genres[i], 0) + plays[i]);
             
             genreMusic.putIfAbsent(genres[i], new ArrayList<>());
             genreMusic.get(genres[i]).add(new Music(i, plays[i]));
         }
         
-        List<String> sortedGenre = genrePlayCount.keySet().stream()
-            .sorted((k1, k2) -> genrePlayCount.get(k2).compareTo(genrePlayCount.get(k1)))
-            .collect(Collectors.toList());
-        
+        List<String> sortedGenre = new ArrayList<>(genrePlayCount.keySet());
+        sortedGenre.sort((a, b) -> 
+                         genrePlayCount.get(b).compareTo(genrePlayCount.get(a)));
+
         List<Integer> answer = new ArrayList<>();
+        
         for (String genre : sortedGenre) {
             List<Music> musics = genreMusic.get(genre);
-            musics.sort((m1, m2) -> {
-                if (m1.play == m2.play) {
-                    return m1.id - m2.id;
+            
+            musics.sort((a, b) -> {
+                if (a.play == b.play) {
+                    return a.id - b.id;
                 }
-                return m2.play - m1.play;
+                return b.play - a.play;
             });
+            
             answer.add(musics.get(0).id);
+            
             if (musics.size() > 1) {
                 answer.add(musics.get(1).id);
             }
