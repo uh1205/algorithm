0426c84408d8d1e266f67416e6f8841298e50d61
Unho Yeo <yuh001205@gmail.com>
2026-03-27 14:04:23 +0900

    [level 2] Title: 바이러스 파이프, Time: 6.54 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -1,78 +1,83 @@
 import java.util.*;
 
 class Solution {
-    int n, k, ans = 0;
-    List<List<Edge>> graph = new ArrayList<>();
-    
-    public int solution(int n, int infection, int[][] edges, int k) {
-        this.n = n;
-        this.k = k;
-        
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int[] e : edges) {
-            int x = e[0], y = e[1], t = e[2];
-            graph.get(x).add(new Edge(y, t));
-            graph.get(y).add(new Edge(x, t));
-        }
-        
-        Set<Integer> infected = new HashSet<>();
-        infected.add(infection);
-        
-        dfs(0, 0, infected);
-        
-        return ans;
-    }
-    
-    void dfs(int depth, int cur, Set<Integer> infected) {
-        ans = Math.max(ans, infected.size());
-        
-        if (depth == k) {
-            return;
-        }
-        
-        for (int i = 1; i <= 3; i++) {
-            if (cur != i) {
-                Set<Integer> newInfected = bfs(i, infected);
-                
-                if (newInfected.size() == infected.size()) continue;
-                
-                dfs(depth + 1, i, newInfected);
-            }
-        }
-    }
-    
-    Set<Integer> bfs(int pipe, Set<Integer> infected) {
-        Queue<Integer> q = new ArrayDeque<>();
-        
-        for (int i = 1; i <= n; i++) {
-            if (infected.contains(i)) q.offer(i);
-        }
-        
-        Set<Integer> newInfected = new HashSet(infected);
-        
-        while (!q.isEmpty()) {
-            int cur = q.poll();
 
-            for (Edge e : graph.get(cur)) {
-                if (e.type == pipe && !newInfected.contains(e.to)) {
-                    q.offer(e.to);
-                    newInfected.add(e.to);
-                }
-            }
-        }
-        
-        return newInfected;
-    }
-    
     static class Edge {
         int to, type;
-        
         Edge(int to, int type) {
             this.to = to;
             this.type = type;
         }
+    }
+
+    List<Edge>[] graph;
+    int n, k;
+    int answer = 0;
+
+    public int solution(int n, int infection, int[][] edges, int k) {
+        this.n = n;
+        this.k = k;
+
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            int a = e[0], b = e[1], t = e[2];
+            graph[a].add(new Edge(b, t));
+            graph[b].add(new Edge(a, t));
+        }
+
+        Set<Integer> start = new HashSet<>();
+        start.add(infection);
+
+        dfs(0, start, 0);
+
+        return answer;
+    }
+
+    void dfs(int depth, Set<Integer> infected, int lastType) {
+        answer = Math.max(answer, infected.size());
+
+        if (depth == k) return;
+
+        for (int type = 1; type <= 3; type++) {
+            if (type == lastType) continue;
+
+            Set<Integer> next = spread(infected, type);
+
+            // 변화 없으면 skip
+            if (next.size() == infected.size()) continue;
+
+            dfs(depth + 1, next, type);
+        }
+    }
+
+    Set<Integer> spread(Set<Integer> infected, int type) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
+
+        for (int node : infected) {
+            q.add(node);
+            visited[node] = true;
+        }
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (Edge e : graph[cur]) {
+                if (e.type != type) continue;
+                if (visited[e.to]) continue;
+
+                visited[e.to] = true;
+                q.add(e.to);
+            }
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) result.add(i);
+        }
+
+        return result;
     }
 }
