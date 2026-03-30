1074f03560eb6488db7bfa703a52401523208c3c
Unho Yeo <yuh001205@gmail.com>
2026-03-30 16:51:27 +0900

    [level 2] Title: 소수 찾기, Time: 20.24 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,33 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> numberSet = new HashSet<>();
-
+    Set<Integer> set = new HashSet<>();
+    boolean[] visited;
+    
     public int solution(String numbers) {
-        // 1. 모든 숫자 조합 생성 (재귀 순열)
-        recursive("", numbers);
-
-        int count = 0;
-        // 2. 생성된 숫자 중 소수 개수 파악
-        for (int num : numberSet) {
-            if (isPrime(num)) count++;
-        }
-
-        return count;
+        visited = new boolean[numbers.length()];
+        dfs("", numbers);
+        return set.size();
     }
-
-    // 모든 가능한 숫자를 조합하는 재귀 함수
-    private void recursive(String current, String others) {
-        // 현재까지 만들어진 숫자가 있다면 Set에 추가
-        if (!current.equals("")) {
-            numberSet.add(Integer.parseInt(current));
-        }
-
-        // 남은 숫자들로 새로운 조합 만들기
-        for (int i = 0; i < others.length(); i++) {
-            recursive(current + others.charAt(i), 
-                      others.substring(0, i) + others.substring(i + 1));
+    
+    void dfs(String cur, String numbers) {
+        for (int i = 0; i < numbers.length(); i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            
+            String next = cur + numbers.charAt(i);
+            int val = Integer.parseInt(next);
+            if (!set.contains(val) && isPrime(val)) {
+                set.add(val);
+            }
+            
+            dfs(next, numbers);
+            visited[i] = false;
         }
     }
-
-    // 소수 판별 함수
-    private boolean isPrime(int n) {
-        if (n < 2) return false; // 0과 1은 소수가 아님
-        
-        // 에라토스테네스의 체 원리: 제곱근까지만 확인
+    
+    boolean isPrime(int n) {
+        if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
         }
