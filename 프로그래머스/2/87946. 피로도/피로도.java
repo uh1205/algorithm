77bb6f7468051c0bdcd1781fe9bd39aeec5ff334
Unho Yeo <yuh001205@gmail.com>
2026-03-30 16:09:55 +0900

    [level 2] Title: 피로도, Time: 4.12 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,20 @@
 class Solution {
     int ans = 0;
-    int n;
-    int[][] dun;
     boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        n = dungeons.length;
-        dun = dungeons;
-        visited = new boolean[n];
-        dfs(0, k);
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
         return ans;
     }
     
-    void dfs(int depth, int left) {
+    void dfs(int depth, int left, int[][] dungeons) {
         ans = Math.max(ans, depth);
         
-        for (int i = 0; i < n; i++) {
-            if (visited[i] || left < dun[i][0]) continue;
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || left < dungeons[i][0]) continue;
             visited[i] = true;
-            dfs(depth + 1, left - dun[i][1]);
+            dfs(depth + 1, left - dungeons[i][1], dungeons);
             visited[i] = false;
         }
     }
