cc3597aca6efbc1df2fc13bc4f0e226a63ae92de
Unho Yeo <yuh001205@gmail.com>
2026-03-30 17:05:22 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.96 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> graph = new ArrayList<>();
-    boolean[] visited;
-    
     public int solution(int n, int[][] edge) {
+        List<List<Integer>> graph = new ArrayList<>();
+        
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
@@ -12,14 +11,12 @@ class Solution {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-        visited = new boolean[n + 1];
-        return bfs(1);
-    }
-    
-    int bfs(int start) {
+        
         Queue<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
+        boolean[] visited = new boolean[n + 1];
+        
+        q.offer(1);
+        visited[1] = true;
         
         int count = 0;
         while (!q.isEmpty()) {
