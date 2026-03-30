5a93c0d18a6c0350e19dd5e4f0c1f8b4f05a1ba3
Unho Yeo <yuh001205@gmail.com>
2026-03-30 14:33:42 +0900

    [level 2] Title: 모음 사전, Time: 5.90 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -5,22 +5,15 @@ class Solution {
     List<String> words = new ArrayList<>();
     
     public int solution(String word) {
-        for (int len = 1; len <= 5; len++) {
-            dfs(new StringBuilder(), len, 0);
-        }
-        words.sort(null);
-        return words.indexOf(word) + 1;
+        dfs("");
+        return words.indexOf(word);
     }
     
-    void dfs(StringBuilder sb, int len, int depth) {
-        if (depth == len) {
-            words.add(sb.toString());
-            return;
-        }
+    void dfs(String cur) {
+        words.add(cur);
+        if (cur.length() == 5) return;
         for (int i = 0; i < 5; i++) {
-            sb.append(moeum[i]);
-            dfs(sb, len, depth + 1);
-            sb.deleteCharAt(depth);
+            dfs(cur + moeum[i]);
         }
     }
 }
