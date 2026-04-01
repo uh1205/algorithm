fa3c65e898d808d54ce50417fa76b4a2d8cbfd9e
Unho Yeo <yuh001205@gmail.com>
2026-04-01 14:36:19 +0900

    [Silver II] Title: 나무 자르기, Time: 448 ms, Memory: 167848 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] tree = new int[N];
+
+        int low = 0;
+        int high = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            high = Math.max(high, tree[i]);
+        }
+
+        // 높이를 mid로 설정했을 때, 얻은 나무가 M 이상인가?
+        // 이상이면 up, 미만이면 down
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long get = 0;
+
+            for (int t : tree) {
+                if (t > mid) {
+                    get += t - mid;
+                }
+            }
+
+            if (get >= M) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
