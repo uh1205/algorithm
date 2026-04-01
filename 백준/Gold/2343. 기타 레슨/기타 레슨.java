ac556d806a586a9e4267740d6133010d4778aad7
Unho Yeo <yuh001205@gmail.com>
2026-04-01 14:57:22 +0900

    [Gold V] Title: 기타 레슨, Time: 224 ms, Memory: 22344 KB -BaekjoonHub

---
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        long low = 0; // 강의 중 최댓값
+        long high = 0; // 강의 총합
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            low = Math.max(low, arr[i]);
+            high += arr[i];
+        }
+
+        // 블루레이 크기를 mid로 설정했을 때, 필요한 블루레이 개수가 M개 이하인가?
+        long ans = high;
+
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            
+            int count = 1;
+            long sum = 0;
+
+            for (int a : arr) {
+                if (sum + a > mid) {
+                    count++;
+                    sum = a;
+                } else {
+                    sum += a;
+                }
+            }
+
+            if (count <= M) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
