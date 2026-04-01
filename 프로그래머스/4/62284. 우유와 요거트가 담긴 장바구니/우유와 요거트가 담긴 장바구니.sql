58cf02dc23f19f4c2ff20f9db42851b426350de8
Unho Yeo <yuh001205@gmail.com>
2026-04-01 19:41:10 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,9 +1,6 @@
 select cart_id
-from (
-    select distinct cart_id, name
-    from cart_products
-    where name in ('Milk', 'Yogurt')
-) as a
+from cart_products
+where name in ('Milk', 'Yogurt')
 group by cart_id
-having count(*) >= 2
+having count(distinct name) = 2
 order by cart_id
