08985c89f83639ee4d5842493b78b6bba6185f28
Unho Yeo <yuh001205@gmail.com>
2026-04-01 15:45:27 +0900

    [level 4] Title: 징검다리, Time: 91.08 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -2,21 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        int ans = 0;
-
         Arrays.sort(rocks);
         
-        int low = 1;
-        int high = distance;
+        long low = 1;
+        long high = distance;
+        
+        // 최소 거리를 mid로 설정했을 때, 제거해야 하는 바위의 개수가 n 이하?
+        // 최댓값 구하기
+        long ans = 0;
         
         while (low <= high) {
-            int mid = (low + high) >>> 1;
+            long mid = (low + high) >>> 1;
             
-            int prev = 0;
             int removed = 0;
+            int prev = 0;
             
             for (int rock : rocks) {
-                if (rock - prev < mid) {
+                if (rock - prev < mid) { // 최소 거리보다 짧으면 제거
                     removed++;
                 } else {
                     prev = rock;
@@ -26,21 +28,14 @@ class Solution {
                 removed++;
             }
             
-            if (removed > n) {
-                high = mid - 1;
-            } else {
-                ans = Math.max(ans, mid);
+            if (removed <= n) {
+                ans = mid;
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         
-        return ans;
+        return (int) ans;
     }
-    
-    // 바위를 n개 제거했을 때 거리의 최솟값 중 최댓값을 구하라
-    // 거리의 최솟값을 K로 설정했을 때, 제거해야 하는 바위의 수가 n개 이하인가?
-    
-    // 설정한 최소 거리 mid보다 짧은 간격이 생기면, 해당 바위를 제거
-    // 제거한 바위의 개수가 n보다 많다면, mid 값을 낮춰야 함
-    // 제거한 바위의 개수가 n 이하라면, 더 큰 최솟값도 가능한지 확인하기 위해 mid 값을 높임
 }
