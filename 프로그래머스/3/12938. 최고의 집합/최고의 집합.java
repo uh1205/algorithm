5df2699e691ba2c04281565a258ec8f3fca61e5f
Unho Yeo <yuh001205@gmail.com>
2026-04-02 21:54:57 +0900

    [level 3] Title: 최고의 집합, Time: 0.31 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,26 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int s) {
-        if (n == 1) {
-            return new int[]{s};
-        }
-        
         if (n > s) {
             return new int[]{-1};
         }
         
         int[] ans = new int[n];
         
-        Arrays.fill(ans, s / n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = s / n;
+        }
         
-        for (int i = n - 1; i >= n - s % n; i--) {
-            ans[i]++;
+        for (int i = 0; i < s % n; i++) {
+            ans[n - 1 - i]++;
         }
         
         return ans;
     }
 }
-// 8
-// 2 2 2 ... 2
-// 2 3 3
