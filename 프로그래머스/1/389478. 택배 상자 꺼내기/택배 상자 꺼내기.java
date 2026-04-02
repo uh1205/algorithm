041a8cbd957b82052056a5d9b403b7e58526ea6d
Unho Yeo <yuh001205@gmail.com>
2026-04-02 22:45:16 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,42 +1,33 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int h = (int) Math.ceil((double) n / w);
+        int targetR = (num - 1) / w;
+        int targetC = getCol(num, w, targetR);
         
-        int[][] arr = new int[h][w];
+        int lastR = (n - 1) / w;
+        int lastC = getCol(n, w, lastR);
         
-        boolean right = true;
-        int r = 0, c = 0;
-        int x = 0, y = 0;
+        int ans = (lastR - 1) - targetR + 1;
         
-        for (int i = 1; i <= n; i++) {
-            arr[r][c] = i;
-            if (i == num) {
-                x = r;
-                y = c;
-            }
-            if (right) {
-                c++;
-            } else {
-                c--;
-            }
-            if (c < 0) {
-                r++;
-                c = 0;
-                right = true;
-            }
-            else if (c >= w) {
-                r++;
-                c = w - 1;
-                right = false;
-            }
+        if (targetC == lastC) return ans + 1;
+        
+        boolean right = false;
+        if (lastR % 2 == 0) {
+            right = true;
         }
         
-        int ans = 0;
-        while (x < h && arr[x][y] > 0) {
-            ans++;
-            x++;
-        }
+        if (targetC < lastC && right) return ans + 1;
+        if (targetC > lastC && !right) return ans + 1;
         
         return ans;
+    }
+    
+    int getCol(int num, int w, int row) {
+        int pos = (num - 1) % w;
+        
+        if (row % 2 == 0) {
+            return pos;
+        } else {
+            return (w - 1) - pos;
+        }
     }
 }
