19ed720c7d2fa9af14f6145961caa4da45ccdbd0
Unho Yeo <yuh001205@gmail.com>
2026-04-03 20:10:55 +0900

    [level 2] Title: 삼각 달팽이, Time: 55.26 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -2,30 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        if (n == 1) return new int[]{1};
-        
         int[][] arr = new int[n][n];
-        
-        int[] dr = {1, 0, -1};
-        int[] dc = {0, 1, -1};
-        
-        int r = 0, c = 0, d = 0, num = 1;
-        
-        while (true) {
-            arr[r][c] = num++;
             
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-            
-            if (nr < 0 || nc < 0 || nr >= n || nc >= n || arr[nr][nc] != 0) {
-                d = (d + 1) % 3;
+        int r = -1, c = 0;
+        int num = 1;
+        
+        for (int i = 0; i < n; i++) { // 단계
+            for (int j = 0; j < n - i; j++) { // 이동 거리
+                if (i % 3 == 0) {
+                    r++;
+                } else if (i % 3 == 1) {
+                    c++;
+                } else {
+                    r--;
+                    c--;
+                }
+                arr[r][c] = num++;
             }
-            
-            r += dr[d];
-            c += dc[d];
-            
-            if (arr[r][c] != 0) break;
         }
+        
+        // int last = n * (1 + n) / 2; // 등차수열 합
         
         List<Integer> ans = new ArrayList<>();
         
