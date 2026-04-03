5d5ea0af7bda10078f4183f202e1c0a13fb64208
Unho Yeo <yuh001205@gmail.com>
2026-04-03 20:11:26 +0900

    [level 2] Title: 삼각 달팽이, Time: 13.07 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -2,38 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        int[][] arr = new int[n][n];
-            
-        int r = -1, c = 0;
+        int[][] triangle = new int[n][n];
+        int maxNum = n * (n + 1) / 2; // 채워야 할 총 숫자의 개수
+        int[] answer = new int[maxNum];
+
+        int r = -1, c = 0; // 시작 지점 보정 (첫 이동이 '하'이므로 r=0이 되도록 -1에서 시작)
         int num = 1;
-        
-        for (int i = 0; i < n; i++) { // 단계
-            for (int j = 0; j < n - i; j++) { // 이동 거리
-                if (i % 3 == 0) {
+
+        for (int i = 0; i < n; i++) { // i는 이동하는 '단계' (n번 방향을 바꿈)
+            for (int j = i; j < n; j++) { // 각 단계마다 이동 거리가 n, n-1, n-2... 순으로 줄어듦
+                if (i % 3 == 0) { // 하
                     r++;
-                } else if (i % 3 == 1) {
+                } else if (i % 3 == 1) { // 우
                     c++;
-                } else {
+                } else { // 좌상 대각선
                     r--;
                     c--;
                 }
-                arr[r][c] = num++;
+                triangle[r][c] = num++;
             }
         }
-        
-        // int last = n * (1 + n) / 2; // 등차수열 합
-        
-        List<Integer> ans = new ArrayList<>();
-        
-        for (int[] row : arr) {
-            for (int v : row) {
-                if (v == 0) {
-                    break;
-                }
-                ans.add(v);
+
+        // 2차원 배열에서 채워진 숫자만 순서대로 1차원 배열에 담기
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer[idx++] = triangle[i][j];
             }
         }
-        
-        return ans.stream().mapToInt(i -> i).toArray();
+
+        return answer;
     }
 }
