620e00a33a3cdb2a02fe708537a09d009c8333d4
Unho Yeo <yuh001205@gmail.com>
2026-04-03 23:32:09 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 24.08 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,25 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int t = 0;
-        int w = 0;
-        int len = 0;
-        int idx = 0;
-        int cnt = 0;
-        int n = truck_weights.length;
-        
         Queue<Integer> q = new ArrayDeque<>();
         
-        while (cnt < n) {
+        for (int i = 0; i < bridge_length; i++) {
+            q.offer(0);
+        }
+
+        int t = 0;
+        int w = 0;
+        int idx = 0;
+        
+        while (idx < truck_weights.length) {
             t++;
-            if (len == bridge_length) {
-                int poll = q.poll();
-                if (poll > 0) cnt++;
-                w -= poll;
-            } else {
-                len++;
-            }
-            if (idx < n && w + truck_weights[idx] <= weight) {
+            w -= q.poll();
+            
+            if (w + truck_weights[idx] <= weight) {
                 int truck = truck_weights[idx++];
                 q.offer(truck);
                 w += truck;
@@ -29,6 +25,6 @@ class Solution {
             }
         }
         
-        return t;
+        return t + bridge_length;
     }
 }
